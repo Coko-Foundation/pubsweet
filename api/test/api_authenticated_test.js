a629bcda89f602612a62c68a216f6506a138c53c
Jure Triglav <juretriglav@gmail.com>
2016-02-07 18:29:35 +0100

    Add failing test for trying to create a fragment in the collection, even though you're authenticated as an unauthorized user.

--- api/test/api_authenticated_test.js
+++ api/test/api_authenticated_test.js
@@ -1,11 +1,11 @@
 const request = require('supertest-as-promised')
 const expect = require('expect.js')
-
 const _ = require('lodash')
 const objectAssign = require('object-assign')
 const dbCleaner = require('./helpers/db_cleaner')
 const fixtures = require('./fixtures/fixtures')
 const userFixture = fixtures.user
+const otherUserFixture = fixtures.updatedUser
 const collectionFixture = fixtures.collection
 const fragmentFixture = fixtures.fragment
 const updatedFragmentFixture = fixtures.updatedFragment
@@ -23,6 +23,13 @@ describe('authenticated api', function () {
         userFixture.email,
         userFixture.password,
         collectionFixture.title)
+    }).then(function () { // Create another user without any roles
+      const User = require('../models/user')
+      return new User({
+        username: otherUserFixture.username,
+        email: otherUserFixture.email,
+        password: otherUserFixture.password
+      }).save()
     }).then(function () {
       api = require('../api')
     })
@@ -43,6 +50,24 @@ describe('authenticated api', function () {
           .send(fragmentFixture)
           .set('Authorization', 'Bearer ' + token)
           .expect(201)
+      })
+  })
+
+  it('fails to create a fragment in a protected collection if authenticated as another user', function () {
+    return request(api)
+      .post('/api/users/authenticate')
+      .send({
+        username: otherUserFixture.username,
+        password: otherUserFixture.password
+      })
+      .expect(201)
+      .then(function (res) {
+        var token = res.body.token
+        return request(api)
+          .post('/api/collection/fragment')
+          .send(fragmentFixture)
+          .set('Authorization', 'Bearer ' + token)
+          .expect(401)
       })
   })
 
