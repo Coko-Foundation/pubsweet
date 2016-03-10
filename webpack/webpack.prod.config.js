cc85b8dc71c359fd6b1bab932d7435c14603d90e
Jure Triglav <juretriglav@gmail.com>
2016-03-10 23:02:42 +0100

    Sensible production webpack configuration.

--- webpack/webpack.prod.config.js
+++ webpack/webpack.prod.config.js
@@ -1,31 +1,53 @@
 var path = require('path')
+var webpack = require('webpack')
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 var assetsPath = path.join(__dirname, '..', 'public', 'assets')
-var publicPath = 'assets/'
+var publicPath = '/assets/'
 
+// We're including JSX components from our components package,
+// but excluding its node_modules.
 var commonLoaders = [
   {
     test: /\.js$|\.jsx$/,
-    loader: 'babel-loader?stage=0',
-    include: path.join(__dirname, '..', 'app')
+    loaders: ['react-hot', 'babel-loader?stage=0'],
+    include: [
+      path.join(__dirname, '..', 'app'),
+      path.join(__dirname, '..', 'routes'),
+      path.join(__dirname, '..', 'app.js'),
+      path.join(__dirname,
+        '..',
+        'node_modules',
+        'pubsweet-substance-components'
+      )
+    ],
+    exclude: [
+      path.join(__dirname,
+        '..',
+        'node_modules',
+        'pubsweet-substance-components',
+        'node_modules'
+      )
+    ]
   },
-  { test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader' },
-  { test: /\.jpg$/, loader: 'file-loader' },
+  { test: /\.png$/, loader: 'url-loader' },
+  {
+    test: /\.woff|\.woff2|\.svg|.eot|\.ttf/,
+    loader: 'url?prefix=font/&limit=10000'
+  },
+  // { test: /\.(jpg|woff|woff2|eot|ttf|svg)$/, loader: 'file-loader' },
   { test: /\.html$/, loader: 'html-loader' },
-  { test: /\.scss$/,
-    loader: 'css/locals?module&localIdentName=[local]%20[hash:base64:5]' +
-      '&sourceMap!sass?sourceMap&outputStyle=expanded' +
-      '&includePaths[]=' + (path.resolve(__dirname, '../node_modules'))
-  }
+  { test: /\.json$/, loader: 'json-loader' }
 ]
 
 module.exports = [
   {
     // The configuration for the client
-    name: 'browser',
+    name: 'app',
+    target: 'web',
     context: path.join(__dirname, '..', 'app'),
     entry: {
-      app: './client'
+      app: [ './app' ]
     },
     output: {
       // The output directory as absolute path
@@ -34,29 +56,32 @@ module.exports = [
       filename: '[name].js',
       // The output path from the view of the Javascript
       publicPath: publicPath
-
     },
-    devtool: 'source-map',
     module: {
       preLoaders: [{
         test: /\.js$|\.jsx$/,
         exclude: /node_modules/,
-        loaders: ['eslint']
+        loaders: ['eslint-loader']
       }],
-      loaders: commonLoaders
+      loaders: commonLoaders.concat([
+          { test: /\.scss$|\.css$/,
+            loader: 'style!css!sass'
+          }
+      ])
     },
     resolve: {
-      extensions: ['', '.react.js', '.js', '.jsx', '.scss'],
-      modulesDirectories: [
-        'app', 'node_modules'
-      ]
+      extensions: ['', '.js', '.jsx', '.json', '.scss']
     },
     plugins: [
       new webpack.DefinePlugin({
         'process.env.NODE_ENV': JSON.stringify('production')
       }),
-      new webpack.ExtractTextPlugin('styles/main.css'),
-      new webpack.optimize.UglifyJsPlugin()
-    ]
+      new ExtractTextPlugin('styles/main.css'),
+      new webpack.optimize.UglifyJsPlugin(),
+      new webpack.NoErrorsPlugin()
+    ],
+    node: {
+      fs: 'empty'
+    }
   }
 ]
