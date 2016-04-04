36e704f640300f848692663f8b4bb7494f819bf5
Jure Triglav <juretriglav@gmail.com>
2016-04-04 13:49:46 +0200

    Make theme loader less verbos in the most common case, using the default theme.

--- webpack/theme-loader.js
+++ webpack/theme-loader.js
@@ -1,28 +1,29 @@
 var loaderUtils = require('loader-utils')
 const fs = require('fs')
 
-module.exports = function (defaultTheme) {
+module.exports = function (defaultStyle) {
   this.cacheable && this.cacheable()
 
   var query = loaderUtils.parseQuery(this.query)
+  var theme = query.theme
   var callback = this.async()
-  console.log(query)
-  console.log(this.resourcePath)
 
   var pathWithTheme = this.resourcePath
 
-  if (query.theme !== 'default') {
+  if (theme !== 'default') {
     var pathWithoutFiletype = this.resourcePath.substr(0, this.resourcePath.lastIndexOf('.'))
-    pathWithTheme = pathWithoutFiletype + '-' + query.theme + '.scss'
+    pathWithTheme = pathWithoutFiletype + '-' + theme + '.scss'
   }
 
-  fs.readFile(pathWithTheme, function (err, theme) {
+  fs.readFile(pathWithTheme, function (err, style) {
     if (err) {
       console.log('No theme found:', pathWithTheme, 'Loading default style.')
-      callback(null, defaultTheme)
+      callback(null, defaultStyle)
     } else {
-      console.log('Theme found:', pathWithTheme)
-      callback(null, theme)
+      if (theme !== 'default') {
+        console.log('Theme found:', pathWithTheme)
+      }
+      callback(null, style)
     }
   })
 }
