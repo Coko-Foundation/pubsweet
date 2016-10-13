16c70aebd8229f15196fbe3ddc655742d9038cbc
Richard Smith-Unna <richardsmith404@gmail.com>
2016-10-13 16:14:37 +0300

    Breakout Login component actions+reducers

--- src/reducers/current_user.js
+++ src/reducers/current_user.js
@@ -2,7 +2,6 @@
 const localStorage = window.localStorage || undefined
 
 import {
-  LOGIN_REQUEST, LOGIN_SUCCESS, LOGIN_FAILURE, LOGOUT_SUCCESS, LOGOUT_REQUEST,
   GET_USER_REQUEST, GET_USER_SUCCESS, GET_USER_FAILURE, SIGNUP_SUCCESS
 } from '../actions/types'
 
@@ -17,35 +16,6 @@ export default function currentUser (state = {
         isFetching: true,
         isAuthenticated: true,
         user: action.user
-      })
-    case LOGIN_REQUEST:
-      return Object.assign({}, state, {
-        isFetching: true,
-        isAuthenticated: false,
-        username: action.credentials.username
-      })
-    case LOGIN_SUCCESS:
-      return Object.assign({}, state, {
-        isFetching: false,
-        isAuthenticated: true,
-        user: action.user,
-        token: action.token
-      })
-    case LOGIN_FAILURE:
-      return Object.assign({}, state, {
-        isFetching: false,
-        isAuthenticated: false,
-        error: action.message
-      })
-    case LOGOUT_SUCCESS:
-      return Object.assign({}, state, {
-        isFetching: false,
-        isAuthenticated: false
-      })
-    case LOGOUT_REQUEST:
-      return Object.assign({}, state, {
-        isFetching: false,
-        isAuthenticated: false
       })
     case GET_USER_REQUEST:
       return Object.assign({}, state, {
