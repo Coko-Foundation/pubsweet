f75fef36844b92d3bb26fa0121d48df14762e966
Jure Triglav <juretriglav@gmail.com>
2016-10-19 17:32:54 +0200

    Add missing require for config to webpack's config

--- initial-app/webpack/webpack.dev.config.js
+++ initial-app/webpack/webpack.dev.config.js
@@ -1,5 +1,6 @@
 var path = require('path')
 var webpack = require('webpack')
+var config = require('config')
 var ThemeResolver = require('./theme-resolver')
 var assetsPath = path.join(__dirname, '..', 'public', 'assets')
 var publicPath = '/assets/'
