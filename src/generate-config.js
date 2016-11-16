a6b62d171472c40fa185a41cb4aa21404bbf87b7
Rik Smith-Unna <rik@fathomlabs.io>
2016-11-16 18:56:21 +0300

    Remove config loading hangover

--- src/generate-config.js
+++ src/generate-config.js
@@ -50,24 +50,12 @@ const write = (path, content) => new Promise(
   )
 )
 
-const loadconfig = () => new Promise(
-  (resolve, reject) => {
-    logger.info('Generated app config files')
-    // load config now so it uses the NODE_CONFIG_DIR we set
-    require('../src/load-config')(path.join(process.cwd(), 'config'))
-    require('config')
-    return resolve()
-  }
-)
-
 module.exports = () => {
   fs.mkdirsSync(path.join(process.cwd(), 'config'))
   return write(
     configpath('dev'), configfile('dev')
   ).then(
     () => write(configpath('production'), configfile('production'))
-  ).then(
-    loadconfig
   ).catch(
     err => {
       logger.error(err.stack)
