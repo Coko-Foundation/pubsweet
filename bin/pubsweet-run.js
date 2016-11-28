a0f04ea91215f608bdbd238acc859a63ca38d117
Rik Smith-Unna <rik@fathomlabs.io>
2016-11-28 16:10:41 +0300

    Check database exists before app run

--- bin/pubsweet-run.js
+++ bin/pubsweet-run.js
@@ -3,6 +3,7 @@
 const forever = require('forever-monitor')
 const program = require('commander')
 const logger = require('../src/logger')
+const fs = require('fs')
 const path = require('path')
 
 program
@@ -15,59 +16,95 @@ process.env.NODE_ENV = program.dev ? 'dev' : 'production'
 let appname = program.args[0]
 if (!appname) appname = process.cwd()
 appname = path.resolve(appname)
-process.chdir(appname)
-
-require('../src/load-config')(path.resolve('', './config'))
-
-logger.info('Starting PubSweet app:', appname)
-
-const logpath = type => path.join(
-  appname,
-  'logs',
-  process.env.NODE_ENV,
-  `${type}.log`
-)
-
-const child = new (forever.Monitor)(
-  path.join(__dirname, '../src/start.js'),
-  {
-    command: 'node',
-    silent: false,
-    watch: false,
-    max: 10,
-    logFile: logpath('forever'),
-    outFile: logpath('stdout'),
-    errFile: logpath('stderr'),
-    cwd: process.cwd(),
-    env: { NODE_ENV: process.env.NODE_ENV }
+const dbCheckPath = path.join(appname, 'api', 'db', process.env.NODE_ENV, 'CURRENT')
+console.log(dbCheckPath)
+const checkExists = () => new Promise(
+  (resolve, reject) => {
+    fs.stat(appname, (err, stats) => {
+      if (err) return reject(err)
+      if (stats.isDirectory()) return resolve()
+      logger.error('Path must be a directory containing a pubsweet app')
+      logger.error('The path you provided is not a directory')
+      logger.error('To generate a new app, see `pubsweet help new`')
+      process.exit(1)
+    })
   }
 )
 
-child.on(
-  'start',
-  (proc, data) => {
-    logger.info(`App ${appname} started.`)
-    logger.info('The app will be kept running, even if errors occur, until you stop it.')
-    logger.info('To stop the app use ctrl-C')
-    logger.info(`Logs will be written to ${appname}/logs/${process.env.NODE_ENV}/{stdout, stderr}.log`)
+const checkDb = () => new Promise(
+  (resolve, reject) => {
+    fs.stat(dbCheckPath, (err, stats) => {
+      if (err) {
+        logger.error('Database appears not to exist')
+        logger.error('To create the database for an existing app, see `pubsweet help setupdb`')
+        logger.error('To generate a new app, see `pubsweet help new`')
+        process.exit(1)
+      } else {
+        return resolve()
+      }
+    })
   }
 )
 
-child.on(
-  'stop',
-  proc => logger.info(`App ${appname} stopped (${proc})`)
+checkExists().then(
+  checkDb
+).then(
+  () => {
+    process.chdir(appname)
+
+    require('../src/load-config')(path.resolve('', './config'))
+
+    logger.info('Starting PubSweet app:', appname)
+
+    const logpath = type => path.join(
+      appname,
+      'logs',
+      process.env.NODE_ENV,
+      `${type}.log`
+    )
+
+    const child = new (forever.Monitor)(
+      path.join(__dirname, '../src/start.js'),
+      {
+        command: 'node',
+        silent: false,
+        watch: false,
+        max: 10,
+        logFile: logpath('forever'),
+        outFile: logpath('stdout'),
+        errFile: logpath('stderr'),
+        cwd: process.cwd(),
+        env: { NODE_ENV: process.env.NODE_ENV }
+      }
+    )
+
+    child.on(
+      'start',
+      (proc, data) => {
+        logger.info(`App ${appname} started.`)
+        logger.info('The app will be kept running, even if errors occur, until you stop it.')
+        logger.info('To stop the app use ctrl-C')
+        logger.info(`Logs will be written to ${appname}/logs/${process.env.NODE_ENV}/{stdout, stderr}.log`)
+      }
+    )
+
+    child.on(
+      'stop',
+      proc => logger.info(`App ${appname} stopped (${proc})`)
+    )
+
+    child.on(
+      'restart',
+      forever => logger.warn(`Restarting ${appname} for ${child.times} time`)
+    )
+
+    child.on(
+      'exit:code',
+      code => logger.error(`Detected ${appname} exited with code ${code}`)
+    )
+
+    child.on('error', err => logger.error(err.stack))
+
+    child.start()
+  }
 )
-
-child.on(
-  'restart',
-  forever => logger.warn(`Restarting ${appname} for ${child.times} time`)
-)
-
-child.on(
-  'exit:code',
-  code => logger.error(`Detected ${appname} exited with code ${code}`)
-)
-
-child.on('error', err => logger.error(err.stack))
-
-child.start()
