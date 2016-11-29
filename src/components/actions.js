4ef8484b4a8272ef31d9f89cf3bde2a04cb2bbc6
Rik Smith-Unna <rik@fathomlabs.io>
2016-11-29 14:22:10 +0300

    Components are functions

--- src/components/actions.js
+++ src/components/actions.js
@@ -1,5 +1,9 @@
 module.exports = PUBSWEET_COMPONENTS.filter(
   component => component.frontend && component.frontend.actions
 ).map(
-  component => component.frontend.actions
+  component => {
+    const actions = component.frontend.actions
+    if (typeof actions === 'function') return actions()
+    else return actions.map(r => r())
+  }
 )
--- src/components/reducers.js
+++ src/components/reducers.js
@@ -1,5 +1,9 @@
 module.exports = PUBSWEET_COMPONENTS.filter(
   component => component.frontend && component.frontend.reducers
 ).map(
-  component => component.frontend.reducers
+  component => {
+    const reducers = component.frontend.reducers
+    if (typeof reducers === 'function') return reducers()
+    else return reducers.map(r => r())
+  }
 )
