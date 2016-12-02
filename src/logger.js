8960fee12d7d312552572d341d30a7871fa74913
Rik Smith-Unna <rik@fathomlabs.io>
2016-12-02 17:12:54 +0300

    Allow console log suppression by env var

--- src/logger.js
+++ src/logger.js
@@ -4,12 +4,17 @@ const winston = require('winston')
 
 var logger
 
+var normalTransports = [
+  new (winston.transports.File)({ filename: 'pubsweet.log' })
+]
+
+if (!process.env.PUBSWEET_BACKEND_SILENT) {
+  normalTransports.push(new (winston.transports.Console)())
+}
+
 if (process.env.NODE_ENV !== 'test') {
   logger = new (winston.Logger)({
-    transports: [
-      new (winston.transports.Console)(),
-      new (winston.transports.File)({ filename: 'pubsweet.log' })
-    ]
+    transports: normalTransports
   })
 } else {
   logger = new (winston.Logger)({
