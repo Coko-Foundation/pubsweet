96495c2a3eb489875f16e681385cdf51305e4d0c
Jure Triglav <juretriglav@gmail.com>
2016-12-16 18:44:02 +0100

    Add validations to Fragment

--- src/models/Fragment.js
+++ src/models/Fragment.js
@@ -1,5 +1,6 @@
 'use strict'
 const Model = require('./Model')
+const Joi = require('joi')
 
 class Fragment extends Model {
   constructor (properties) {
@@ -10,5 +11,19 @@ class Fragment extends Model {
 }
 
 Fragment.type = 'fragment'
+
+Fragment.schema = Joi.object().keys({
+  id: Joi.string().guid().required(),
+  type: Joi.string().required(),
+  title: Joi.string(),
+  rev: Joi.string(),
+  owners: Joi.array().items(Joi.string().guid()),
+  // TODO: These are emergent, any way we can standardize?
+  source: Joi.string(),
+  kind: Joi.string(),
+  presentation: Joi.string(),
+  published: Joi.boolean()
+
+})
 
 module.exports = Fragment
