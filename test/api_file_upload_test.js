1d40984565bfc8bad48f57c9090433b17af4af34
Jure Triglav <juretriglav@gmail.com>
2017-01-07 15:44:31 +0100

    Transform AVA test to Jest

--- test/api_file_upload_test.js
+++ test/api_file_upload_test.js
@@ -1,4 +1,3 @@
-import test from 'ava'
 import fs from 'fs'
 import path from 'path'
 
@@ -8,10 +7,10 @@ function file (name) {
   return fs.createReadStream(path.join(__dirname, '..', 'src', 'test', 'fixtures', name))
 }
 
-test('should upload a file and preserve the extension', t => {
+it('should upload a file and preserve the extension', () => {
   return api.upload.post(file('fixture.jpg')).then(res => {
-    t.is(res.statusCode, 200)
-    t.is(path.extname(res.text), '.jpg')
+    expect(res.statusCode).toBe(200)
+    expect(path.extname(res.text)).toBe('.jpg')
   })
 })
 
