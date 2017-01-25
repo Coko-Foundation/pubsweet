75854b2015dff5f4d9ec65a525af5d2cd7b11b31
Alf Eaton <eaton.alf@gmail.com>
2017-01-25 09:42:34 +0000

    Move /api/upload route handler to a separate file

---
+++ src/routes/api_upload.js
@@ -0,0 +1,27 @@
+const path = require('path')
+const crypto = require('crypto')
+const multer = require('multer')
+const express = require('express')
+const api = express.Router()
+
+const storage = multer.diskStorage({
+  destination: 'uploads/',
+  filename: function (req, file, cb) {
+    crypto.pseudoRandomBytes(16, function (err, raw) {
+      if (err) return cb(err)
+
+      cb(null, raw.toString('hex') + path.extname(file.originalname))
+    })
+  }
+})
+
+const upload = multer({
+  storage: storage,
+  limits: {fileSize: 10000000, files: 1}
+})
+
+api.post('', upload.single('file'), (req, res, next) => {
+  return res.send('/' + req.file.path)
+})
+
+module.exports = api
