54b8a2571fdc517fc8ef0b4332cb964a7bf45dcb
Alf Eaton <eaton.alf@gmail.com>
2017-01-25 15:22:52 +0000

    Remove API route that exports all docs as JSON

--- src/routes/api.js
+++ src/routes/api.js
@@ -37,21 +37,4 @@ api.use('/users', users)
 const teams = require('./api_teams')
 api.use('/teams', teams)
 
-// Debug API
-if (process.env.NODE_ENV === 'dev') {
-  api.get('/debug', (req, res, next) => {
-    db.allDocs({
-      include_docs: true,
-      attachments: true
-    }).then(result => {
-      var documents = result.rows.map(result => {
-        return result.doc
-      })
-      return res.json(documents)
-    }).catch(err => {
-      console.log(err)
-    })
-  })
-}
-
 module.exports = api
