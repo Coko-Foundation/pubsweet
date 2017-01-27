e7331e1201939f9afcddf84e1681d70e10b55be8
Alf Eaton <eaton.alf@gmail.com>
2017-01-27 11:47:37 +0000

    Write to a log file per environment
    Write logs to a "logs" directory, with a separate log for each
    environment.

--- src/logger.js
+++ src/logger.js
@@ -1,28 +1,25 @@
 'use strict'
 
+const path = require('path')
 const winston = require('winston')
 
-var logger
-
-var normalTransports = [
-  new (winston.transports.File)({ filename: 'pubsweet.log' })
+// write to a log file per environment
+let transports = [
+  new (winston.transports.File)({
+    filename: path.join(__dirname, '..', 'logs', 'pubsweet_' + process.env.NODE_ENV + '.log')
+  })
 ]
 
+// write to the console if not silent and not testing
 if (!process.env.PUBSWEET_BACKEND_SILENT) {
-  normalTransports.push(new (winston.transports.Console)())
+  if (process.env.NODE_ENV !== 'test') {
+    transports.push(new (winston.transports.Console)())
+  }
 }
 
-if (process.env.NODE_ENV !== 'test') {
-  logger = new (winston.Logger)({
-    transports: normalTransports
-  })
-} else {
-  logger = new (winston.Logger)({
-    transports: [
-      new (winston.transports.File)({ filename: 'test.log' })
-    ]
-  })
-}
+const logger = new (winston.Logger)({
+  transports: transports
+})
 
 logger.stream = {
   write: (message, encoding) => {
