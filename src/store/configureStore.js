8a964d8317a31068005c0e9460aca2065c4b2685
Jure Triglav <juretriglav@gmail.com>
2017-02-09 16:13:34 +0100

    Move globals (CONFIG/VALIDATIONS) from webpack config into the store configuration.

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,3 +1,9 @@
+// Cross-platform global: https://www.contentful.com/blog/2017/01/17/the-global-object-in-javascript/
+(function (global) {
+  global.CONFIG = require(process.env.APP_DIR + '/config/' + process.env.NODE_ENV)
+  global.VALIDATIONS = require('pubsweet-backend/src/models/validations')(global.CONFIG)
+}).call(this, typeof global !== 'undefined' ? global : typeof self !== 'undefined' ? self : typeof window !== 'undefined' ? window : {})
+
 if (process.env.NODE_ENV === 'production') {
   module.exports = require('./configureStore.prod')
 } else {
