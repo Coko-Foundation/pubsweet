5d89d8bb57570c888b3b0aa1b527ca171bb867e1
Jure Triglav <juretriglav@gmail.com>
2017-02-09 16:17:38 +0100

    Use the global module instead of own code to access global namespace.

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,8 +1,6 @@
-// Cross-platform global: https://www.contentful.com/blog/2017/01/17/the-global-object-in-javascript/
-(function (global) {
-  global.CONFIG = require(process.env.APP_DIR + '/config/' + process.env.NODE_ENV)
-  global.VALIDATIONS = require('pubsweet-backend/src/models/validations')(global.CONFIG)
-}).call(this, typeof global !== 'undefined' ? global : typeof self !== 'undefined' ? self : typeof window !== 'undefined' ? window : {})
+var global = require('global')
+global.CONFIG = require(process.env.APP_DIR + '/config/' + process.env.NODE_ENV)
+global.VALIDATIONS = require('pubsweet-backend/src/models/validations')(global.CONFIG)
 
 if (process.env.NODE_ENV === 'production') {
   module.exports = require('./configureStore.prod')
