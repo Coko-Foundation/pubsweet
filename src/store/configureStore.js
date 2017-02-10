d12d8556743ab72bbe3a5e3d368eebdca5178a5a
Jure Triglav <juretriglav@gmail.com>
2017-02-10 17:50:17 +0100

    Actually this IIFE is only executed after requires, and we need CONFIG for certain requires, so CONFIG needs to come from higher up = webpacks's ProvidePlugin

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,7 +1,3 @@
-var global = require('global')
-global.CONFIG = require(process.env.APP_DIR + '/config/' + process.env.NODE_ENV)
-global.VALIDATIONS = require('pubsweet-backend/src/models/validations')(global.CONFIG)
-
 if (process.env.NODE_ENV === 'production') {
   module.exports = require('./configureStore.prod')
 } else {
