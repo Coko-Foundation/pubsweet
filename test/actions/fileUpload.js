8f8b4b0f3a9aa3fb98f2c3e35d85b60061b15ae9
Rik Smith-Unna <rik@fathomlabs.io>
2017-02-17 13:59:40 +0300

    Debugging fileUpload

--- test/actions/fileUpload.js
+++ test/actions/fileUpload.js
@@ -1,3 +1,8 @@
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 600000
+process.env.NODE_ENV = 'production'
+process.env.SILENT_NPM = 'true'
+global.PUBSWEET_COMPONENTS = []
+
 const expect = require.requireActual('chai').expect
 
 global.FormData = require('formdata-polyfill')
@@ -7,9 +12,12 @@ const actions = require.requireActual('../../src/actions/fileUpload')
 const describeAction = require.requireActual('../helpers/describeAction')(actions)
 const T = require('../../src/actions/types')
 
+const uploadpath = require('path').resolve('.', 'fileUpload.js')
+console.log('uploadpath: ', uploadpath)
+
 module.exports = app => {
   describeAction('fileUpload', {
-    firstarg: require('path').join('.', 'fileUpload.js'),
+    firstarg: uploadpath,
     types: {
       request: T.FILE_UPLOAD_REQUEST,
       success: T.FILE_UPLOAD_SUCCESS,
@@ -22,6 +30,9 @@ module.exports = app => {
     },
     user: app.user
   }, (action, data) => {
+    if (data[T.FILE_UPLOAD_FAILURE]) {
+      console.log('ERROR:', data[T.FILE_UPLOAD_FAILURE].error)
+    }
     // optional: more functionality tests here
   })
 }
--- test/helpers/cleanup.js
+++ test/helpers/cleanup.js
@@ -6,6 +6,16 @@ const mockdatafile = path.join(__dirname, '..', 'helpers', 'mockapp.json')
 
 const mockdata = require(mockdatafile)
 
+const logfile = path.join(mockdata.server, 'logs', 'production', 'stderr.log')
+
+try {
+  fs.statSync(logfile)
+  console.log('APP ERROR LOG:')
+  console.log(fs.readFileSync(logfile))
+} catch (e) {
+  console.log('No app error log to show')
+}
+
 fs.unlinkSync(mockdatafile)
 
 process.kill(-mockdata.pid)
