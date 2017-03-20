6e8aaa01c28abeabb44f403e05adcbd8d7012e84
Alf Eaton <eaton.alf@gmail.com>
2017-03-20 21:49:27 +0000

    Provide UpdateSubscriber component

---
+++ src/components/UpdateSubscriber.jsx
@@ -0,0 +1,75 @@
+import { connect } from 'react-redux'
+import React, { Component, PropTypes } from 'react'
+import * as T from '../actions/types'
+
+const actionMap = {
+  'fragment:create': T.CREATE_FRAGMENT_SUCCESS,
+  'fragment:replace': T.UPDATE_FRAGMENT_SUCCESS,
+  'fragment:update': T.UPDATE_FRAGMENT_SUCCESS,
+  'fragment:delete': T.DELETE_FRAGMENT_SUCCESS,
+}
+
+class UpdateSubscriber extends Component {
+  componentDidMount () {
+    this.subscribe(this.props)
+  }
+
+  componentWillReceiveProps (nextProps) {
+    if (!this.eventSource) {
+      this.subscribe(nextProps)
+    }
+  }
+
+  componentWillUnmount () {
+    if (this.eventSource) {
+      this.eventSource.close()
+      // delete this.eventSource
+    }
+  }
+
+  subscribe (props) {
+    const {currentUser, handleUpdate} = props
+
+    if (currentUser) {
+      // EventSource can't have Authorization header, so have to use query string
+      const url = '/updates?access_token=' + encodeURIComponent(currentUser.token)
+
+      this.eventSource = new EventSource(url)
+
+      this.eventSource.onerror = e => console.error(e)
+
+      this.eventSource.onmessage = e => {
+        const {action, data} = JSON.parse(e.data)
+
+        const actionType = actionMap[action]
+
+        handleUpdate(actionType, data)
+      }
+    }
+  }
+
+  render () {
+    const {currentUser} = this.props
+
+    // TODO: show if authenticated + connected
+    // TODO: pass eventSource to another component for display?
+
+    return (
+      <div>{ currentUser ? ':)' : '' }</div>
+    )
+  }
+}
+
+UpdateSubscriber.propTypes = {
+  currentUser: PropTypes.object,
+  storeUpdate: PropTypes.func,
+}
+
+export default connect(
+  state => ({
+    currentUser: state.currentUser,
+  }),
+  dispatch => ({
+    handleUpdate: (type, body) => dispatch({type, ...body})
+  })
+)(UpdateSubscriber)
