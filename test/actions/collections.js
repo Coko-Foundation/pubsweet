2f39ba8576e35716a223f174de7b8e9311c1c254
Rik Smith-Unna <rik@fathomlabs.io>
2017-04-03 22:29:39 +0300

    wip: test multiple collections actions

--- test/actions/collections.js
+++ test/actions/collections.js
@@ -13,9 +13,80 @@ module.exports = app => {
     types: {
       request: T.GET_COLLECTIONS_REQUEST,
       success: T.GET_COLLECTIONS_SUCCESS
+      // TODO: there's no failure mode right now because collections
+      // can be listed without login - when we add more failure
+      // modes than auth fail we should enable failure here
     },
     properties: {
-      success: ['collections']
+      request: ['type'],
+      success: ['type', 'collections', 'receivedAt'],
+      failure: ['type', 'error']
+    },
+    user: () => app.user
+  }, (action, data) => {
+    // optional: more functionality tests here
+  })
+
+  let newcol
+
+  describeAction('createCollection', {
+    firstarg: {
+      type: 'testing',
+      title: 'this is a test collection'
+    },
+    types: {
+      request: T.CREATE_COLLECTION_REQUEST,
+      success: T.CREATE_COLLECTION_SUCCESS,
+      failure: T.CREATE_COLLECTION_FAILURE
+    },
+    properties: {
+      request: ['type', 'collection'],
+      success: ['type', 'collection'],
+      failure: ['type', 'isFetching', 'collection', 'error']
+    },
+    user: () => app.user
+  }, (action, data) => {
+    newcol = data.CREATE_COLLECTION_SUCCESS.collection
+    // optional: more functionality tests here
+  })
+
+  // TODO: updateCollection
+
+  describeAction('patchCollection', {
+    firstarg: () => newcol,
+    secondarg: {
+      type: 'testing',
+      title: 'this is a changed collection'
+    },
+    types: {
+      request: T.PATCH_COLLECTION_REQUEST,
+      success: T.PATCH_COLLECTION_SUCCESS,
+      failure: T.PATCH_COLLECTION_FAILURE
+    },
+    properties: {
+      request: ['type', 'collection'],
+      success: ['type', 'collection'],
+      failure: ['type', 'isFetching', 'collection', 'error']
+    },
+    user: () => app.user
+  }, (action, data) => {
+    expect(
+      data.CREATE_COLLECTION_SUCCESS.collection.title
+    ).to.not.equal(newcol.title)
+    // optional: more functionality tests here
+  })
+
+  describeAction('deleteCollection', {
+    firstarg: () => newcol,
+    types: {
+      request: T.DELETE_COLLECTION_REQUEST,
+      success: T.DELETE_COLLECTION_SUCCESS,
+      failure: T.DELETE_COLLECTION_FAILURE
+    },
+    properties: {
+      request: ['type', 'collection', 'update'],
+      success: ['type', 'collection'],
+      failure: ['type', 'collection', 'update', 'error']
     },
     user: () => app.user
   }, (action, data) => {
