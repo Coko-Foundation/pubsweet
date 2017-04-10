d3bd9804070137f159998b711cf5d40c41085a9a
Rik Smith-Unna <rik@fathomlabs.io>
2017-04-10 15:50:16 +0300

    apply flow fix to setup-db test, enable env-based test

--- test/setup-db.js
+++ test/setup-db.js
@@ -1,4 +1,5 @@
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 600000
+require('./helpers/fix_stdio')
 
 require('app-module-path').addPath(__dirname + '/../..')
 
@@ -6,48 +7,54 @@ const expect = require('chai').expect
 const fs = require('fs-extra')
 const path = require('path')
 const workingdir = require('./helpers/working_dir')
+const logger = require('../src/logger')
 
 const appname = 'testapp'
 
 const dbconfig = require('./fixtures').dbconfig
 
-const env = process.env.NODE_ENV
+const env = 'production'
 
 describe('setup-db', () => {
   let appdir
   let dbdir
   const dbfiles = env => fs.readdirSync(path.join(dbdir, env))
 
-  beforeAll(() => workingdir().then(
-    tmpdir => {
-      appdir = path.join(tmpdir, appname)
-      dbdir = path.join(appdir, 'api', 'db')
-      fs.mkdirsSync(appdir)
-      process.chdir(appname)
+  beforeAll(async (done) => {
+    const tmpdir = await workingdir()
+    appdir = fs.mkdirsSync(path.join(tmpdir, appname))
+    dbdir = path.join(appdir, 'api', 'db')
+    process.chdir(appdir)
+    logger.info('created directory')
 
-      return require('../src/generate-config')().then(
-        require('../src/generate-env')
-      ).then(
-        () => require('../src/initial-app')(appname)
-      ).then(
-        require('../src/setup-db')({
-          properties: require('../src/db-properties'),
-          override: dbconfig
-        })
-      )
+    await require('../src/generate-config')()
+    logger.info('config generated')
+
+    await require('../src/generate-env')()
+    logger.info('env generated')
+
+    await require('../src/initial-app')(appname)
+    logger.info('app generated')
+
+    await require('../src/setup-db')({
+      properties: require('../src/db-properties'),
+      override: dbconfig
+    })
+    logger.info('db created')
+
+    done()
+  })
+
+  it('creates the database',
+    () => {
+      expect(fs.readdirSync(dbdir)).to.include(env)
+      expect(dbfiles(env)).to.include.members([
+        'CURRENT',
+        'LOG',
+        'LOCK'
+      ])
     }
-  ))
-
-  // it('creates the database',
-  //   () => {
-  //     expect(fs.readdirSync(dbdir)).to.include(env)
-  //     expect(dbfiles('production')).to.include.members([
-  //       'CURRENT',
-  //       'LOG',
-  //       'LOCK'
-  //     ])
-  //   }
-  // )
+  )
 
   it('only creates the database for the current NODE_ENV',
     () => {
