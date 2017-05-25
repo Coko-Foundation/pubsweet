4100bbfcd4de95dfbf20a80d17a45255c31662f9
Alf Eaton <eaton.alf@gmail.com>
2017-05-25 09:46:56 +0100

    Add pubsweet as a dependency of initial-app

--- src/initial-app.js
+++ src/initial-app.js
@@ -11,6 +11,7 @@ const getpkgjson = name => {
     description: 'A new pubsweet app',
     dependencies: {
       'joi': '^10.4.1',
+      'pubsweet': '^0.3.3',
       'pubsweet-server': '^0.9.2',
       'pubsweet-client': '^0.9.1',
       'pubsweet-component-blog': '^0.1.5',
@@ -67,6 +68,10 @@ const getpkgjson = name => {
       'webpack': '^2.4.1',
       'webpack-dev-middleware': '^1.10.2',
       'webpack-hot-middleware': '^2.18.0'
+    },
+    scripts: {
+      start: 'pubsweet run', // `npm start -- --dev` for dev mode
+      pubsweet: 'pubsweet' // `npm run pubsweet -- setupdb . --dev` to setup the dev db
     },
     repository: 'put your repo here',
     license: 'UNLICENSED'
