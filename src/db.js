04d675056d9ce27d066e92114849afb7ba239459
Alf Eaton <eaton.alf@gmail.com>
2017-05-30 12:57:55 +0100

    Create a new in-memory db each time

--- src/db.js
+++ src/db.js
@@ -1,39 +1,45 @@
-const uuid = require('uuid')
 const path = require('path')
+const uuid = require('uuid')
 
 const PouchDB = require('pouchdb-core')
   .plugin(require('pouchdb-find'))
   .plugin(require('pouchdb-upsert'))
   .plugin(require('relational-pouch'))
 
-const config = require('../config')
-
-let name, adapter
-
-switch (process.env.NODE_ENV) {
-  case 'test':
-    PouchDB.plugin(require('pouchdb-adapter-memory'))
-
-    adapter = 'memory'
-    name = uuid()
-
-    break
-
-  default:
-    PouchDB.plugin(require('pouchdb-adapter-http')).plugin(require('pouchdb-adapter-leveldb'))
-
-    const dbPath = config['pubsweet-server']['dbPath']
-
-    if (dbPath.match(/^http:/)) {
-      adapter = 'http'
-      name = dbPath
-    } else {
-      adapter = 'leveldb'
-      name = path.join(dbPath, process.env.NODE_ENV)
-    }
-    break
+if (process.env.NODE_ENV === 'test') {
+  PouchDB.plugin(require('pouchdb-adapter-memory'))
+} else {
+  PouchDB.plugin(require('pouchdb-adapter-http')).plugin(require('pouchdb-adapter-leveldb'))
 }
 
+const dbPath = require('../config')['pubsweet-server']['dbPath']
+
+const dbAdapter = (dbPath) => {
+  // In-memory test db
+  if (process.env.NODE_ENV === 'test') return 'memory'
+
+  // HTTP URL, e.g. CouchDB
+  if (dbPath.match(/^http:/)) return 'http'
+
+  // local file system
+  return 'leveldb'
+}
+
+const dbName = (adapter) => {
+  switch (adapter) {
+    case 'memory':
+      return uuid()
+
+    case 'http':
+      return dbPath
+
+    case 'leveldb':
+      return path.join(dbPath, process.env.NODE_ENV)
+  }
+}
+
+const adapter = dbAdapter(dbPath)
+
 module.exports = () => {
-  return new PouchDB(name, { adapter })
+  return new PouchDB(dbName(adapter), { adapter })
 }
