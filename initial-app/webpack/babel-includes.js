170b64df97aa7f1a53769961b33fb590eca1ac2d
Jure Triglav <juretriglav@gmail.com>
2017-07-26 15:52:58 +0200

    Fix Regex

--- initial-app/webpack/babel-includes.js
+++ initial-app/webpack/babel-includes.js
@@ -3,5 +3,5 @@ const path = require('path')
 module.exports = [
   new RegExp(path.join(__dirname, '..', 'node_modules', 'pubsweet-client', 'src')),
   new RegExp(path.join(__dirname, '..', 'app')),
-  new RegExp(path.join(__dirname, '..', 'node_modules', 'pubsweet-[^/\\]*(?!.*/node_modules)'))
+  new RegExp(path.join(__dirname, '..', 'node_modules', 'pubsweet-[^/\\\\]*(?!.*/node_modules)'))
 ]
