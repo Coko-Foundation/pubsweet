ba178f882e627d79b74157b6bd5534c734936543
Samuel Galson <samgalson@gmail.com>
2017-08-22 11:30:30 +0100

    feature: set logger by config

--- test/index.test.js
+++ test/index.test.js
@@ -1,10 +1,17 @@
+'use strict'
+
+process.env.ALLOW_CONFIG_MUTATIONS = true
+process.env.SUPPRESS_NO_CONFIG_WARNING = true
+
+let config = require('config')
+config['pubsweet-server'] = { }
 
 describe('Logging manager', () => {
   describe('when no logger is specifed', () => {
 
     it('logs errors to console', () => {
       jest.spyOn(global.console, 'error').mockImplementation()
-      const logger = require('../src')
+      const logger = require('../src/')
       logger.error('an error message')
       expect(console.error).toHaveBeenCalled()
       console.error.mockRestore()
@@ -12,7 +19,7 @@ describe('Logging manager', () => {
 
     it('logs warn to console', () => {
       jest.spyOn(global.console, 'warn').mockImplementation()
-      const logger = require('../src')
+      const logger = require('../src/')
       logger.warn('a warn message')
       expect(console.warn).toHaveBeenCalled()
       console.warn.mockRestore()
@@ -20,7 +27,7 @@ describe('Logging manager', () => {
 
     it('logs info to console', () => {
       jest.spyOn(global.console, 'info').mockImplementation()
-      const logger = require('../src')
+      const logger = require('../src/')
       logger.info('an info message')
       expect(console.info).toHaveBeenCalled()
       console.info.mockRestore()
@@ -28,7 +35,7 @@ describe('Logging manager', () => {
 
     it('logs debug to console', () => {
       jest.spyOn(global.console, 'log').mockImplementation()
-      const logger = require('../src')
+      const logger = require('../src/')
       logger.debug('a debug message')
       expect(console.log).toHaveBeenCalled()
       console.log.mockRestore()
@@ -36,8 +43,8 @@ describe('Logging manager', () => {
 
   })
 
-  describe('when passed another logger', () => {
-    const logger = require('../src')
+  describe('when configure method is passed another logger', () => {
+    const logger = require('../src/')
 
     it('throws an error if a required method is not implemented', () => {
       expect(() => {
@@ -84,13 +91,57 @@ describe('Logging manager', () => {
   })
 
   describe('has getRawLogger method', () => {
-    const logger = require('../src')
+    const logger = require('../src/')
 
     it('which returns raw logger', () => {
       const bunyan = require('bunyan').createLogger({name: 'test'})
       logger.configure(bunyan)
       const rawLogger = logger.getRawLogger()
       expect(rawLogger.fields.name).toBe('test')
+    })
+
+  })
+
+  describe('config ', () => {
+
+    it('sets logger to "bunyan" if specified', () => {
+      jest.resetModules();
+      config = require('config')
+      config['pubsweet-server'] = { logger: 'bunyan' }
+      const logger = require('../src/')
+      const rawLogger = logger.getRawLogger()
+      expect(rawLogger.fields.name).toBe('pubsweet-logger')
+    })
+
+    it('sets logger to "winston" if specified', () => {
+      jest.resetModules();
+      config = require('config')
+      config['pubsweet-server'] = { logger: 'winston' }
+      const logger = require('../src/')
+      const rawLogger = logger.getRawLogger()
+      expect(rawLogger).toEqual(require('winston'))
+    })
+
+    it('defaults to console', () => {
+      jest.resetModules();
+      config = require('config')
+      config['pubsweet-server'] = {}
+      const logger = require('../src/')
+      const rawLogger = logger.getRawLogger()
+      expect(rawLogger).toEqual(console)
+    })
+
+    it('accepts "bunyan", "winston" or "undefined" only', () => {
+      jest.resetModules();
+      config = require('config')
+      config['pubsweet-server'] = { logger: 'wiiiiiiiiinston' }
+
+      // https://github.com/facebook/jest/issues/2124
+      try {
+        require('../src/')
+      } catch (e) {
+        expect(e.name).toBe('ValidationError')
+      }
     })
 
   })
