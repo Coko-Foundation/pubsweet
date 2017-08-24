8bc63751c5bd11992e42f7c248cc1c5dc14c41c4
Tamlyn Rhodes <tamlyn@tamlyn.org>
2017-08-24 17:52:24 +0100

    Add sample component tests

---
+++ packages/Navigation/Navigation.test.jsx
@@ -0,0 +1,31 @@
+import {shallow} from 'enzyme'
+import React from 'react'
+
+import Navigation from './Navigation'
+
+global.CONFIG = {'pubsweet-client': {}}
+
+describe('<Navigation/>', () => {
+  const makeWrapper = (props = {}) => shallow(<Navigation
+      actions={{logoutUser: jest.fn()}}
+      currentUser={{isAuthenticated: true, user: {}}}
+      {...props}
+  />)
+
+  it('shows user nav when logged in', () => {
+    const wrapper = makeWrapper()
+    expect(wrapper.find('NavbarUser')).toHaveLength(1)
+  })
+
+  it("doesn't show user nav when logged out", () => {
+    const wrapper = makeWrapper({currentUser: {isAuthenticated: false, user: {}}})
+    expect(wrapper.find('NavbarUser')).toHaveLength(0)
+  })
+
+  it('triggers logout action', () => {
+    const logoutUser = jest.fn()
+    const wrapper = makeWrapper({actions: {logoutUser}})
+    wrapper.find('NavbarUser').dive().find('.logout').simulate('click')
+    expect(logoutUser).toHaveBeenCalled()
+  })
+})
