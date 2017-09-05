483674be9440bf23c899d139fb72d389c358d52d
Alf Eaton <eaton.alf@gmail.com>
2017-09-05 10:01:48 +0100

    Check for existence of config

--- src/components.js
+++ src/components.js
@@ -3,6 +3,8 @@ const logger = require('./logger')
 const config = require(path.join(process.cwd(), 'config/' + process.env.NODE_ENV))
 
 module.exports = app => {
+  if (!config.pubsweet || !config.pubsweet.components) return
+
   config.pubsweet.components.forEach(name => {
     const component = require(path.join(process.cwd(), 'node_modules', name))
     logger.info('Registered component', name)
