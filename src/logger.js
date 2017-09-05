dfe9f2b1265b9dc8976409d177be20c7cdab2757
Alf Eaton <eaton.alf@gmail.com>
2017-09-05 15:59:54 +0100

    Remove logging to files

--- src/logger.js
+++ src/logger.js
@@ -5,9 +5,6 @@ const winston = require('winston')
 
 const logger = new (winston.Logger)({
   transports: [
-    new (winston.transports.File)({
-      filename: path.join(__dirname, '..', 'logs', 'pubsweet_' + process.env.NODE_ENV + '.log')
-    }),
     new (winston.transports.Console)({
       colorize: true
     })
