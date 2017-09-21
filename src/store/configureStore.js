7ac45cf26e246ac9f4bcc7f9004aaa1af14eb472
Jure Triglav <juretriglav@gmail.com>
2017-09-21 15:50:51 +0200

    Update to react-router v4, closes #19

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,5 +1,59 @@
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./configureStore.prod')
-} else {
-  module.exports = require('./configureStore.dev')
+import { createStore, combineReducers, compose, applyMiddleware } from 'redux'
+import { routerReducer, routerMiddleware } from 'react-router-redux'
+
+import thunk from 'redux-thunk'
+import { createLogger } from 'redux-logger'
+import { reducer as formReducer } from 'redux-form'
+import config from 'config'
+import reducers from '../reducers'
+
+require('../components/reducers').forEach(
+  componentReducers => Object.assign(reducers, componentReducers)
+)
+
+function configureStore (env) {
+  return function configureStore (history, initialState) {
+    const reducer = combineReducers({
+      ...reducers,
+      form: formReducer,
+      routing: routerReducer
+    })
+
+    let logger
+    if (!config['pubsweet-client']['redux-log'] && env !== 'production') {
+      logger = applyMiddleware(createLogger())
+    }
+
+    const middleware = [
+      applyMiddleware(thunk),
+      logger,
+      applyMiddleware(routerMiddleware(history))
+    ].filter(value => value)
+
+    // https://github.com/zalmoxisus/redux-devtools-extension#12-advanced-store-setup
+    const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+    const store = createStore(
+      reducer,
+      initialState,
+      composeEnhancers(...middleware)
+    )
+
+    if (module.hot) {
+      // Enable Webpack hot module replacement for reducers
+      module.hot.accept('../reducers', () => {
+        const nextRootReducer = require('../reducers')
+        store.replaceReducer(nextRootReducer)
+      })
+    }
+
+    return store
+  }
 }
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports = configureStore('production')
+} else {
+  module.exports = configureStore('development')
+}
+
