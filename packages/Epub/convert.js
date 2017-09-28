5645abe0967979e5b2ee9b92a11ca1cff7b9f1cd
Alf Eaton <eaton.alf@gmail.com>
2017-09-28 16:33:31 +0300

    Convert notes to divs

--- packages/Epub/convert.js
+++ packages/Epub/convert.js
@@ -4,8 +4,17 @@ module.exports = source => {
   const $ = cheerio.load(source)
 
   // remove "uploads" from the start of each img src attribute
-  $('img[src]').each((i, elem) => {
-    elem.attr('src', elem.attr('src').replace(/^\/?uploads\//))
+  $('img[src]').each(() => {
+    const $img = $(this)
+    $img.attr('src', $img.attr('src').replace(/^\/?uploads\//))
+  })
+
+  // replace "note" elements with divs
+  $('note').each(() => {
+    const $note = $(this)
+    const div = $('<div class="note"/>')
+    $note.contents().appendTo(div)
+    $note.replaceWith(div)
   })
 
   return $.html()
