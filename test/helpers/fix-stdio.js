95f6f81db41d559ca7a47930fa1008f2b3913d63
Samuel Galson <samgalson@gmail.com>
2017-09-29 11:28:57 +0100

    add inflection

--- test/helpers/fix-stdio.js
+++
@@ -1,6 +0,0 @@
-// https://github.com/nodejs/node/issues/6379
-// https://github.com/nodejs/node/issues/6456
-const outputs = [process.stdout, process.stderr]
-outputs.forEach(s => {
-  s && s.isTTY && s._handle && s._handle.setBlocking && s._handle.setBlocking(true)
-})
