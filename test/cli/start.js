610ec0512d36a3e4818634c8f3730efe8fd66421
Samuel Galson <samgalson@gmail.com>
2017-09-29 15:28:23 +0100

    add test

--- test/cli/start.js
+++ test/cli/start.js
@@ -7,14 +7,31 @@ jest.mock('webpack', () => {
 jest.mock(require('path').join(process.cwd(), 'webpack', `webpack.${process.env.NODE_ENV}.config.js`), () => {}, {virtual: true})
 jest.mock(require('path').join(process.cwd(), 'config', 'components.json'), () => [], {virtual: true})
 jest.mock('pubsweet-server', () => jest.fn(x => x))
+jest.mock('forever-monitor', () => ({
+  start: jest.fn(() => ({ on: jest.fn() }))
+}))
 
-// const compilerSpy = require('webpack').run
+// const foreverSpy = require('forever-monitor').start
 
+const config = require('config')
+config['pubsweet-server'] = { dbPath: __dirname, adapter: 'leveldb' }
+
+const runStart = require('../../cli/start')
 const start = require('../../src/start/')
 
 describe('start', () => {
+  let server
+
+  afterAll(() => {
+    server.close()
+  })
+
+  it('throws an error if no database found', async () => {
+    await expect(runStart()).rejects.toHaveProperty('message', `Create database with "pubsweet setupdb" before starting app`)
+  })
+
   it('starts the server', async () => {
-    const server = await start()
+    server = await start()
     expect(server.listening).toBe(true)
   })
 })
