4fa1c324568fffeaad954114622a30e39cfedbb0
Samuel Galson <samgalson@gmail.com>
2017-10-02 16:28:26 +0100

    fix flaky start test

--- test/cli/start.js
+++ test/cli/start.js
@@ -12,22 +12,23 @@ jest.mock('forever-monitor', () => ({
 }))
 
 // const foreverSpy = require('forever-monitor').start
-
+const { getMockArgv } = require('../helpers/')
+const Promise = require('bluebird')
 const config = require('config')
 config['pubsweet-server'] = { dbPath: __dirname, adapter: 'leveldb' }
-
 const runStart = require('../../cli/start')
 const start = require('../../src/start/')
 
 describe('start', () => {
   let server
 
-  afterAll(() => {
-    server.close()
+  afterAll(async () => {
+    const closeServer = Promise.promisify(server.close, { context: server })
+    await closeServer()
   })
 
   it('throws an error if no database found', async () => {
-    await expect(runStart()).rejects.toHaveProperty('message', `Create database with "pubsweet setupdb" before starting app`)
+    await expect(runStart(getMockArgv(''))).rejects.toHaveProperty('message', `Create database with "pubsweet setupdb" before starting app`)
   })
 
   it('starts the server', async () => {
