b75ed5f8209ef3a2316191030f36c2c04a7a29c3
Samuel Galson <samgalson@gmail.com>
2017-10-02 14:42:26 +0100

    update linting

--- test/helpers/index.js
+++ test/helpers/index.js
@@ -6,10 +6,10 @@ const formatOpts = reduce((acc, value, key) => acc.concat(`--${key}`, value), []
 const joinArgsAndOpts = (args, options) => (args ? args.split(' ') : []).concat(formatOpts(options))
 
 const getEnvWithConfig = (config = {}) => Object.assign(
-    {},
-    process.env,
-    { NODE_CONFIG: JSON.stringify(config) }
-  )
+  {},
+  process.env,
+  { NODE_CONFIG: JSON.stringify(config) }
+)
 
 const getMockArgv = ({ args, options }) => ['mock/exec/path', 'mock/file/path']
   .concat(joinArgsAndOpts(args, options))
