35044d7acd0aee827ae8d308b3abe728d7e5bebd
Alf Eaton <eaton.alf@gmail.com>
2017-10-10 11:09:38 +0100

    Standardise variable names

--- packages/Epub/converters/wax.js
+++ packages/Epub/converters/wax.js
@@ -9,12 +9,13 @@ module.exports = $ => {
   // replace extracts and epigraphs with "blockquote"
 
   $('extract-prose, extract-poetry, epigraph-poetry, epigraph-prose').each((i, elem) => {
-    const $extract = $(elem)
+    const $elem = $(elem)
 
     const blockquote = $('<blockquote class="sc-blockquote"/>')
-    $extract.contents().appendTo(blockquote)
 
-    $extract.replaceWith(blockquote)
+    $elem.contents().appendTo(blockquote)
+
+    $elem.replaceWith(blockquote)
   })
 
   // replace "note[content]" with "aside" and a link
@@ -23,71 +24,90 @@ module.exports = $ => {
   // })
 
   $('note').each((i, elem) => {
-    const $note = $(elem)
-    const aside = $(
-      `<aside epub:type="endnote" id="${$note.attr('data-id')}" class="note"/>`
-    )
-    aside.html($note.attr('note-content'))
-    $('body').append(aside)
+    const $elem = $(elem)
 
-    const callout = $(
-      `<span href="${$note.attr(
-        'data-id'
-      )}" epub:type="noteref" class="note-callout"/>`
-    )
-    callout.text(i)
-    $note.replaceWith(callout)
+    const id = $elem.attr('data-id')
+
+    $('<aside epub:type="endnote" class="note"/>')
+      .attr('id', id)
+      .html($elem.attr('note-content'))
+      .appendTo('body')
+
+    const callout = $('<span epub:type="noteref" class="note-callout"/>')
+      .attr('href', id) // TODO: needs hash?
+      .text(i)
+
+    $elem.replaceWith(callout)
   })
 
   $('comment').each((i, elem) => {
-    let commentEncloses = $(elem).text()
-    $(elem).replaceWith(commentEncloses)
+    const $elem = $(elem)
+
+    $elem.replaceWith($elem.text())
   })
+
   $('track-change').each((i, elem) => {
-    let trackChangeStatus = $(elem).attr('status')
-    if (trackChangeStatus === 'delete') {
-      let trackChangeEncloses = $(elem).text()
-      $(elem).replaceWith(trackChangeEncloses)
+    const $elem = $(elem)
+
+    if ($elem.attr('status') === 'delete') {
+      $elem.replaceWith($elem.text())
     } else {
-      $(elem).remove()
+      $elem.remove()
     }
   })
+
   $('chapter-number').each((i, elem) => {
-    let chapterNumberEnclosure = $(elem).text()
-    $(elem).replaceWith(
-      `<p class="sc-chapter-number">${chapterNumberEnclosure}</p>`
-    )
+    const $elem = $(elem)
+
+    const p = $('<p class="sc-chapter-number"/>')
+      .text($elem.text())
+
+    $elem.replaceWith(p)
   })
+
   $('chapter-title').each((i, elem) => {
-    let chapterTitleEnclosure = $(elem).text()
-    $(elem).replaceWith(
-      `<p class="sc-chapter-title">${chapterTitleEnclosure}</p>`
-    )
+    const $elem = $(elem)
+
+    const p = $('<p class="sc-chapter-title"/>')
+      .text($elem.text())
+
+    $elem.replaceWith(p)
   })
+
   $('chapter-subtitle').each((i, elem) => {
-    let chapterSubtitleEnclosure = $(elem).text()
-    $(elem).replaceWith(
-      `<p class="sc-chapter-subtitle">${chapterSubtitleEnclosure}</p>`
-    )
+    const $elem = $(elem)
+
+    const p = $('<p class="sc-chapter-subtitle"/>')
+      .text($elem.text())
+
+    $elem.replaceWith(p)
   })
+
   $('source-note').each((i, elem) => {
-    let sourceNoteEnclosure = $(elem).text()
-    $(elem).replaceWith(`<p class="sc-source-note">${sourceNoteEnclosure}</p>`)
+    const $elem = $(elem)
+
+    const p = $('<p class="sc-source-note"/>')
+      .text($elem.text())
+
+    $elem.replaceWith(p)
   })
 
+  // add class name to ordered lists
   $('ol').each((i, elem) => {
-    const $ol = $(elem)
-    if ($ol.attr('styling') === 'qa') {
-      const qa = $('<ol class="sc-list-ol-qa"/>')
-      $ol.contents().appendTo(qa)
+    const $elem = $(elem)
 
-      $ol.replaceWith(qa)
-    }
-    if ($ol.attr('styling') === 'unstyled') {
-      const unstyled = $('<ol class="sc-list-ol-unstyled"/>')
-      $ol.contents().appendTo(unstyled)
+    switch ($elem.attr('styling')) {
+      case 'qa':
+        const qa = $('<ol class="sc-list-ol-qa"/>')
+        $elem.contents().appendTo(qa)
+        $elem.replaceWith(qa)
+        break
 
-      $ol.replaceWith(unstyled)
+      case 'unstyled':
+        const unstyled = $('<ol class="sc-list-ol-unstyled"/>')
+        $elem.contents().appendTo(unstyled)
+        $elem.replaceWith(unstyled)
+        break
     }
   })
 }
