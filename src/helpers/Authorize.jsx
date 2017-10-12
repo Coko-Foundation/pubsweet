a287df6fcb8a77ab2d5bf4e6a40ecf0ad5f78897
Tamlyn Rhodes <tamlyn@tamlyn.org>
2017-10-12 11:52:16 +0100

    Update components to use new authsome version
    Remove redirect on authorization failure from AuthenticatedComponent
    Refactor it to use Authorize component
    Create withAuthsome higher order component to inject authsome on demand
    Shim model fetching functions for authsome to fetch from Redux store
    Update users reducer to add current user to users array so that it can be fetched by shims
    Add jsx to extensions parsed by ESlint

--- src/helpers/Authorize.jsx
+++ src/helpers/Authorize.jsx
@@ -1,49 +1,42 @@
 'use strict'
 
-import Authsome from 'authsome'
 import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-import config from 'config'
+import { compose } from 'redux'
+
+import withAuthsome from './withAuthsome'
 
 export class Authorize extends React.Component {
   constructor (props) {
     super(props)
 
-    // TODO:
-    // Ah, this is because for the client, we supply the
-    // objects with owners in a different format than backend.
-    // And authsome operates with the backend way. Need to rethink.
-    this.object = Object.assign({}, this.props.object)
-    if (Array.isArray(this.object.owners)) {
-      this.object.owners = this.object.owners.map(
-        owner => owner ? owner.id : null
-      )
+    this.state = {
+      authorized: false
     }
-    // /TODO
+  }
 
-    this.authsome = new Authsome(
-      props.authsome.mode,
-      { teams: props.authsome.teams }
-    )
+  componentWillMount () {
+    this.checkAuth(this.props)
+  }
+
+  componentWillReceiveProps (nextProps) {
+    this.checkAuth(nextProps)
+  }
+
+  async checkAuth ({ authsome, currentUser, operation, object }) {
+    try {
+      const authorized = await authsome.can(currentUser && currentUser.id, operation, object)
+      this.setState({
+        authorized
+      })
+    } catch (err) {
+      console.error(err)
+    }
   }
 
   render () {
-    let { currentUser, operation } = this.props
-    let object = this.object
-    let permissions
-
-    try {
-      permissions = this.authsome.can(currentUser, operation, object)
-    } catch (err) {
-      permissions = false
-    }
-
-    if (permissions) {
-      return this.props.children
-    } else {
-      return null
-    }
+    return this.state.authorized ? this.props.children : this.props.unauthorized || null
   }
 }
 
@@ -52,10 +45,8 @@ Authorize.propTypes = {
   operation: PropTypes.string,
   object: PropTypes.object,
   children: PropTypes.node,
-  authsome: PropTypes.shape({
-    mode: PropTypes.func.isRequired,
-    teams: PropTypes.object
-  }).isRequired
+  unauthorized: PropTypes.node,
+  authsome: PropTypes.object.isRequired
 }
 
 function mapState (state) {
@@ -63,10 +54,8 @@ function mapState (state) {
     teams: state.teams,
     collections: state.collections,
     fragments: state.fragments,
-    currentUser: state.currentUser.user,
-    // TODO fix this
-    authsome: config.authsome
+    currentUser: state.currentUser.user
   }
 }
 
-export default connect(mapState)(Authorize)
+export default compose(withAuthsome(), connect(mapState))(Authorize)
--- test/components/AuthenticatedComponent.test.jsx
+++ test/components/AuthenticatedComponent.test.jsx
@@ -7,11 +7,9 @@ const { AuthenticatedComponent } = require('../../src/components/AuthenticatedCo
 
 function makeWrapper (props = {}) {
   return shallow(<AuthenticatedComponent
-    selector={jest.fn(() => ({}))}
+    object={{}}
     operation=""
-    authsome={{ mode: jest.fn(() => false) }}
     actions={{
-      getCollections: () => Promise.resolve(),
       getCurrentUser: () => Promise.resolve()
     }}
     currentUser={{}}
@@ -23,59 +21,29 @@ function makeWrapper (props = {}) {
 }
 
 describe('<AuthenticatedComponent/>', () => {
-  it('does nothing when fetching', () => {
+  it('does nothing when fetching', async () => {
     const pushState = jest.fn()
     const wrapper = makeWrapper({ pushState })
     wrapper.instance().checkAuth({ isFetching: true })
 
-    expect(wrapper.type()).toBe(null)
     expect(pushState).not.toHaveBeenCalled()
   })
 
-  it('redirects to login if not authenticated', () => {
+  it('redirects to login if not authenticated', async () => {
     const pushState = jest.fn()
     const wrapper = makeWrapper({ pushState, location: { pathname: 'blah' } })
     wrapper.instance().checkAuth({ isAuthenticated: false })
 
-    expect(wrapper.type()).toBe(null)
     expect(pushState).toHaveBeenCalledWith('/login?next=blah')
-  })
-
-  it('redirects to authsome fail URL if not authorized', () => {
-    const pushState = jest.fn()
-    const wrapper = makeWrapper({
-      pushState,
-      authsome: {
-        mode: jest.fn(() => false),
-        'fail-redirect': 'stuff'
-      }
-    })
-    wrapper.instance().checkAuth({ isAuthenticated: true })
-
-    expect(wrapper.type()).toBe(null)
-    expect(pushState).toHaveBeenCalledWith('stuff')
-  })
-
-  it('renders children if authorized', () => {
-    const pushState = jest.fn()
-    const wrapper = makeWrapper({
-      pushState,
-      authsome: {
-        mode: jest.fn(() => true),
-        'fail-redirect': 'stuff'
-      }
-    })
-    wrapper.instance().checkAuth({ isAuthenticated: true })
-
-    expect(wrapper.name()).toBe('button')
-    expect(pushState).not.toHaveBeenCalled()
   })
 
   it('calls checkAuth() when props change', () => {
     const pushState = jest.fn()
     const wrapper = makeWrapper({ pushState, location: { pathname: 'blah' } })
-    wrapper.setProps({currentUser: { isAuthenticated: false }})
 
+    expect(pushState).not.toHaveBeenCalled()
+
+    wrapper.setProps({ isAuthenticated: false })
     expect(pushState).toHaveBeenCalledWith('/login?next=blah')
   })
 })
