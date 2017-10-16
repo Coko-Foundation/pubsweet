04375fed5bd85583420f14ca458670770454c0da
Samuel Galson <samgalson@gmail.com>
2017-10-16 16:22:48 +0100

    make store configurable

---
+++ test/store/configureStore.test.js
@@ -0,0 +1,35 @@
+
+describe('configureStore', () => {
+  afterEach(() => jest.resetModules())
+
+  it('allows initialization of store with custom initialState, reducers and middleware', () => {
+    global.PUBSWEET_COMPONENTS = []
+
+    const configureStore = require('../../src/store/configureStore')
+
+    const customHistory = {}
+    const customInitialState = { 'collections': ['initial'] }
+
+    const customReducers = {
+      test: (state, action) => {
+        return {
+          ...state,
+          fired: action.fired
+        }
+      }
+    }
+
+    const customMiddlewares = [
+      () => next => action => {
+        action.fired = true
+        return next(action)
+      }
+    ]
+
+    const store = configureStore(customHistory, customInitialState, customReducers, customMiddlewares)
+
+    expect(store.getState().collections[0]).toBe(customInitialState.collections[0])
+    store.dispatch({ type: 'TEST', fired: false })
+    expect(store.getState().test.fired).toBe(true)
+  })
+})
