cffe02fc653d6c452755185ef9197e00c7ad5f50
Tamlyn Rhodes <tamlyn@tamlyn.org>
2017-10-17 15:27:28 +0100

    Fix DB existence check for HTTP (CouchDB)

--- src/helpers/db-exists.js
+++ src/helpers/db-exists.js
@@ -2,30 +2,30 @@ const path = require('path')
 const fetch = require('isomorphic-fetch')
 const fs = require('fs-extra')
 const logger = require('@pubsweet/logger')
-const dbPath = require('./db-path')
+const getDbPath = require('./db-path')
 
 const httpDbExists = async url => {
   try {
-    await fetch(url)
-    return true
+    const response = await fetch(url)
+    return response.status === 200
   } catch (e) {
     logger.debug('Error requesting DB:', e)
     return false
   }
 }
 
-const dbExists = async () => {
-  if (/^http/.test(dbPath())) {
-    return httpDbExists()
+const dbExists = async (dbPath) => {
+  if (/^http/.test(dbPath)) {
+    return httpDbExists(dbPath)
   }
 
-  const dbCheckPath = path.join(dbPath(), 'CURRENT')
+  const dbCheckPath = path.join(dbPath, 'CURRENT')
   return fs.pathExists(dbCheckPath)
 }
 
 module.exports = async () => {
-  logger.info(`Checking if database exists at path ${dbPath()}.`)
-  const exists = await dbExists()
+  logger.info(`Checking if database exists at ${getDbPath()}.`)
+  const exists = await dbExists(getDbPath())
   logger.info(`Database ${exists ? 'exists' : 'does not exist'}.`)
   return exists
 }
