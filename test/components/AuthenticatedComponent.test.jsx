0c4db3ac8a7541d7fe6fdc338032961450d08400
Jure Triglav <juretriglav@gmail.com>
2017-10-21 15:02:57 +0200

    Add test for rendering children if authenticated/not authenticated

--- test/components/AuthenticatedComponent.test.jsx
+++ test/components/AuthenticatedComponent.test.jsx
@@ -26,6 +26,7 @@ describe('<AuthenticatedComponent/>', () => {
     const wrapper = makeWrapper({ pushState })
     wrapper.instance().checkAuth({ isFetching: true })
 
+    expect(wrapper.find('button').length).toBe(0)
     expect(pushState).not.toHaveBeenCalled()
   })
 
@@ -45,5 +46,12 @@ describe('<AuthenticatedComponent/>', () => {
 
     wrapper.setProps({ isAuthenticated: false })
     expect(pushState).toHaveBeenCalledWith('/login?next=blah')
+  })
+
+  it('renders children components when authenticated', async () => {
+    const pushState = jest.fn()
+    const wrapper = makeWrapper({ pushState, location: { pathname: 'blah' }, isFetching: false, isAuthenticated: true })
+
+    expect(wrapper.find('button').length).toBe(1)
   })
 })
