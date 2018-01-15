5523f420f64681262ef1785ddd59508e77e7fd0e
julientaq <julientaq@gmail.com>
2018-01-15 18:03:45 +0100

    update styling for button, avatar, badge, button, menu, checkbox and radioaffi

--- packages/ui/src/atoms/Radio.js
+++ packages/ui/src/atoms/Radio.js
@@ -2,25 +2,24 @@ import React from 'react'
 import classnames from 'classnames'
 import classes from './Radio.local.scss'
 
-const inputGradient = color =>
-  `radial-gradient(closest-corner at center, ${color} 0%, ${color} 45%,
-    white 45%, white 100%)`
-
 const Radio = ({
   className,
   color = 'black',
   inline,
+  author,
   name,
   value,
   label,
   checked,
   required,
   onChange,
+  readonly,
 }) => (
   <label
     className={classnames(
       classes.root,
       {
+        [classes.author]: author,
         [classes.inline]: inline,
         [classes.checked]: checked,
       },
@@ -31,19 +30,15 @@ const Radio = ({
     <input
       checked={checked}
       className={classes.input}
+      disabled={readonly}
       name={name}
       onChange={onChange}
       required={required}
       type="radio"
       value={value}
-    />
-    <span
-      className={classes.pseudoInput}
-      style={{ background: checked ? inputGradient(color) : 'transparent' }}
-    >
-      {' '}
-    </span>
-    <span className={classes.label}>{label}</span>
+    />{' '}
+    <span className={classes.pseudoInput} style={{ color }} />{' '}
+    <span className={classes.label}> {label} </span>{' '}
   </label>
 )
 
