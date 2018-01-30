72411287702289ca1147b4eb4b1318253d1a8155
Aanand Prasad <aanand.prasad@gmail.com>
2018-01-30 17:22:59 +0000

    feat(ui): convert AlignmentBox to a styled component

--- packages/ui/src/atoms/AlignmentBox.js
+++ packages/ui/src/atoms/AlignmentBox.js
@@ -1,22 +1,36 @@
-import classNames from 'classnames'
 import PropTypes from 'prop-types'
 import React from 'react'
+import styled from 'styled-components'
 
-import classes from './AlignmentBox.local.scss'
+const lightGrey = '#b3b3b3'
+const mainGrey = '#666'
+const whiteRGBA = 'rgba(255, 255, 255, 1)'
 
-const AlignmentBox = ({ active, id, noBorder, onClick }) => {
-  const styles = classNames(classes.root, {
-    [classes.active]: active,
-    [classes.noBorderTop]: noBorder.top,
-    [classes.noBorderRight]: noBorder.right,
-    [classes.noBorderBottom]: noBorder.bottom,
-    [classes.noBorderLeft]: noBorder.left,
-  })
+const Root = styled.div.attrs({
+  role: 'presentation',
+})`
+  box-shadow: inset 0 0 0 2px ${whiteRGBA};
+  cursor: pointer;
+  height: 26px;
+  width: 17px;
 
-  return (
-    <div className={styles} id={id} onClick={onClick} role="presentation" />
-  )
-}
+  border-style: solid;
+  border-color: ${mainGrey};
+  border-top-width: ${props => (props.noBorder.top ? 0 : 1)}px;
+  border-right-width: ${props => (props.noBorder.right ? 0 : 1)}px;
+  border-bottom-width: ${props => (props.noBorder.bottom ? 0 : 1)}px;
+  border-left-width: ${props => (props.noBorder.left ? 0 : 1)}px;
+
+  background-color: ${props => (props.active ? mainGrey : 'transparent')};
+
+  &:hover {
+    background-color: ${lightGrey};
+  }
+`
+
+const AlignmentBox = ({ active, id, noBorder, onClick }) => (
+  <Root active={active} id={id} noBorder={noBorder} onClick={onClick} />
+)
 
 AlignmentBox.propTypes = {
   active: PropTypes.bool.isRequired,
