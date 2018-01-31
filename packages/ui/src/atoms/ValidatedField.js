ca281cc1bff93177eae37e8d46dc95166c606d51
Samuel Galson <samgalson@gmail.com>
2018-01-31 11:11:15 +0000

    feat(ui): use styled-components 2 for jest-styled-components compat

--- packages/ui/src/atoms/ValidatedField.js
+++ packages/ui/src/atoms/ValidatedField.js
@@ -1,10 +1,33 @@
 import React from 'react'
 import { compose, withHandlers } from 'recompose'
-import classnames from 'classnames'
 import { Field } from 'redux-form'
-import classes from './ValidatedField.local.scss'
+import styled from 'styled-components'
 
 // TODO: pass ...props.input to children automatically?
+
+const MessageWrapper = styled.div`
+  font-family: var(--font-author);
+  display: inline-block;
+  font-style: italic;
+  margin-left: 1em;
+  margin-top: 10px;
+`
+
+const Message = styled.div`
+  &:not(:last-child) {
+    margin-bottom: 10px;
+  }
+  font-size: 0.9em;
+  letter-spacing: 0.01em;
+`
+
+const ErrorMessage = Message.extend`
+  color: var(--color-danger);
+`
+
+const WarningMessage = Message.extend`
+  color: var(--color-warning);
+`
 
 const ValidatedFieldComponent = ({ component }) => ({ meta, input }) => (
   <div>
@@ -12,19 +35,10 @@ const ValidatedFieldComponent = ({ component }) => ({ meta, input }) => (
 
     {meta.touched &&
       (meta.error || meta.warning) && (
-        <div className={classes.messages}>
-          {meta.error && (
-            <div className={classnames(classes.message, classes.error)}>
-              {meta.error}
-            </div>
-          )}
-
-          {meta.warning && (
-            <div className={classnames(classes.message, classes.warning)}>
-              {meta.warning}
-            </div>
-          )}
-        </div>
+        <MessageWrapper>
+          {meta.error && <ErrorMessage>{meta.error}</ErrorMessage>}
+          {meta.warning && <WarningMessage>{meta.warning}</WarningMessage>}
+        </MessageWrapper>
       )}
   </div>
 )
