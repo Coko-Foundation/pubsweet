6527724cea5e06c5e164a0b3868d4404f1e6a7d3
Aanand Prasad <aanand.prasad@gmail.com>
2018-02-01 11:51:31 +0000

    feat(ui): convert AppBar to a styled component

--- packages/ui/src/molecules/AppBar.js
+++ packages/ui/src/molecules/AppBar.js
@@ -1,8 +1,76 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import classnames from 'classnames'
-import classes from './AppBar.local.scss'
+import styled from 'styled-components'
+
 import Icon from '../atoms/Icon'
+
+const Root = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  margin: 1rem;
+
+  a {
+    color: var(--color-primary);
+    text-decoration: none;
+
+    &::before {
+      color: #aaa;
+      display: inline-block;
+      height: 1em;
+      margin-right: 0.3em;
+      text-align: center;
+
+      &:hover {
+        color: var(--color-primary);
+      }
+    }
+  }
+`
+
+const Section = styled.div`
+  display: flex;
+`
+
+const NavLinks = styled.div`
+  align-items: center;
+  display: flex;
+  margin: 0 1rem;
+
+  a {
+    padding: 0 1rem;
+
+    &:global(.active) {
+      font-weight: bold;
+    }
+  }
+`
+
+const Logo = styled(Link)`
+  font-weight: bold;
+  text-decoration: none;
+
+  &::before {
+    content: '';
+  }
+`
+
+const itemStyle = `
+  align-items: center;
+  display: inline-flex;
+  padding: 0 1rem;
+
+  svg {
+    margin-right: 0.3rem;
+  }
+`
+
+const Item = styled.span`
+  ${itemStyle};
+`
+const ActionItem = Item.withComponent('a')
+const LinkItem = styled(Link)`
+  ${itemStyle};
+`
 
 const AppBar = ({
   brandLink = '/',
@@ -13,40 +81,32 @@ const AppBar = ({
   user,
   className,
 }) => (
-  <nav className={classnames(classes.root, className)}>
-    <div className={classes.section}>
-      {brand && (
-        <Link className={classes.logo} to={brandLink}>
-          {brand}
-        </Link>
-      )}
+  <Root>
+    <Section>
+      {brand && <Logo to={brandLink}>{brand}</Logo>}
 
-      {navLinks && <div className={classes.navLinks}>{navLinks}</div>}
-    </div>
+      {navLinks && <NavLinks>{navLinks}</NavLinks>}
+    </Section>
 
-    <div className={classes.section}>
+    <Section>
       {user && (
-        <span className={classes.item}>
+        <Item>
           <Icon size={16}>user</Icon>
           {user.username}
           {user.admin ? ' (admin)' : ''}
-        </span>
+        </Item>
       )}
 
       {user && (
-        <a className={classes.item} href="#" onClick={onLogoutClick}>
+        <ActionItem href="#" onClick={onLogoutClick}>
           <Icon size={16}>power</Icon>
           Logout
-        </a>
+        </ActionItem>
       )}
 
-      {!user && (
-        <Link className={classes.item} to={loginLink}>
-          Login
-        </Link>
-      )}
-    </div>
-  </nav>
+      {!user && <LinkItem to={loginLink}>Login</LinkItem>}
+    </Section>
+  </Root>
 )
 
 export default AppBar
