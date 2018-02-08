c589f4ff05a15ee47e1416d92daef5aad62acb43
Samuel Galson <samgalson@gmail.com>
2018-02-08 11:25:04 +0000

    feat(ui): add theming to UploadingFile

--- packages/ui/src/atoms/UploadingFile.js
+++ packages/ui/src/atoms/UploadingFile.js
@@ -7,79 +7,60 @@ const Root = styled.div`
   align-items: center;
   display: inline-flex;
   flex-direction: column;
-  margin-bottom: 2em;
-  margin-right: 3em;
+  margin-bottom: var(--grid-unit);
+  margin-right: calc(var(--sub-grid-unit) * 3);
   position: relative;
-  width: 20ch;
+  width: calc(var(--grid-unit) * 10);
 `
 
 const ErrorWrapper = styled.div`
-  background: var(--color-danger);
-  border: 2px solid white;
-  color: white;
-  font-size: 0.8em;
+  background: var(--color-error);
+  border: calc(var(--border-width) * 2) var(--border-style)
+    var(--color-text-reverse);
+  color: var(--color-text-reverse);
+  font-size: var(--font-size-base-small);
   letter-spacing: 0.01em;
   opacity: 1;
-  padding: 0.3em 0.5em;
+  padding: var(--sub-grid-unit) var(--sub-grid-unit);
   position: absolute;
   top: 25%;
   z-index: 4;
 `
 
 const Icon = styled.div`
-  background: #ddd;
-  height: 100px;
-  margin: 5px;
+  background: var(--color-furniture);
+  height: calc(var(--grid-unit) * 3);
+  margin: 0 var(--sub-grid-unit) var(--sub-grid-unit) var(--sub-grid-unit);
   opacity: 0.5;
   position: relative;
-  width: 70px;
+  width: calc(var(--grid-unit) * 2);
 `
 
 const Progress = styled.div`
-  background-image:
-    linear-gradient(
-      var(--color-primary-light) 50%,
-      var(--color-primary) 75%,
-      to top
-    );
-  bottom: 0;
-  content: '';
+  color: var(--color-text-reverse);
   display: block;
-  left: 0;
-  opacity: 1;
   position: absolute;
-  right: 0;
-  transform-origin: 0 0;
-
-  &::after {
-    /* we can use a data attribute for the numbering below */
-    bottom: 2px;
-    color: white;
-    content: '${props => props.percent}%';
-    display: block;
-    position: absolute;
-    right: 2px;
-  }
+  bottom: var(--sub-grid-unit);
+  left: calc(var(--sub-grid-unit) * 2);
 `
 
 const Filename = styled.div`
-  color: gray;
-  font-size: 90%;
+  color: var(--color-text);
+  font-size: var(--font-size-base-small);
   font-style: italic;
-  margin: 5px;
-  max-width: 20ch;
+  margin: 0 var(--sub-grid-unit) var(--sub-grid-unit) var(--sub-grid-unit);
+  max-width: calc(var(--grid-unit) * 10);
 `
 const Extension = styled.div`
-  background: #888;
-  color: white;
-  font-size: 12px;
-  left: 20px;
-  padding: 2px;
+  background: var(--color-text);
+  color: var(--color-text-reverse);
+  font-size: var(--font-size-base-small);
+  left: calc(var(--sub-grid-unit) * 2);
   position: absolute;
   right: 0;
   text-align: center;
   text-transform: uppercase;
-  top: 20px;
+  top: calc(var(--sub-grid-unit) * 2);
 `
 const extension = ({ name }) => name.replace(/^.+\./, '')
 
@@ -87,7 +68,7 @@ const UploadingFile = ({ file, error, progress }) => (
   <Root>
     {!!error && <ErrorWrapper>{error}</ErrorWrapper>}
     <Icon>
-      {!!progress && <Progress percent={progress * 100} />}
+      {!!progress && <Progress>{progress * 100}%</Progress>}
       <Extension>{extension(file)}</Extension>
     </Icon>
     <Filename>{file.name}</Filename>
