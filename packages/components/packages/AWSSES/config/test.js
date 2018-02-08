2e34627a7203fcac97ca83cd2009a6baf6df601c
Sebastian <sebastian.mihalache@thinslices.com>
2018-02-08 15:50:52 +0200

    feat(component): add aws ses package

---
+++ packages/components/packages/AWSSES/config/test.js
@@ -0,0 +1,8 @@
+module.exports = {
+  'pubsweet-component-aws-ses': {
+    secretAccessKey: 'secret-key',
+    accessKeyId: 'access-key',
+    region: 'region',
+    sender: 'test_sender@domain.com',
+  },
+}
