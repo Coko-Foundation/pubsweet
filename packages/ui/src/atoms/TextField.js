6479df0558659b9187b690dfe9841adc1c88c595
Aanand Prasad <aanand.prasad@gmail.com>
2018-02-09 10:09:08 +0000

    style: apply theming to TextField

--- packages/ui/src/atoms/TextField.js
+++ packages/ui/src/atoms/TextField.js
@@ -2,61 +2,25 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Root = styled.label`
-  --font-local: var(--font-reviewer);
-
-  align-items: center;
   display: flex;
-  font-size: 1em;
-  line-height: 1.8;
+  flex-direction: column;
 `
 
 const Label = styled.span`
-  margin-right: 10px;
+  font-size: var(--font-size-base-small);
 `
 
 const Input = styled.input`
-  --color-back: darkgray;
+  border: var(--border-width) var(--border-style) var(--color-border);
+  border-radius: var(--border-radius);
 
-  background: linear-gradient(
-      to right,
-      transparent 0%,
-      transparent 2px,
-      white 2px,
-      white 4px
-    ),
-    linear-gradient(white 0%, white 90%, var(--color-back) 95%, white 100%);
-  background-position: 0 0, 0.2em 0;
-  background-repeat: repeat-X, repeat-Y;
-  background-size: 7px 100%, 100% 1.3em;
-  border: none;
-  border-left: 1px solid darkgrey;
-  caret-color: var(--color-primary);
-  flex: 1;
-  font-family: var(--font-local), serif;
   font-size: inherit;
-  max-width: 100%;
-  padding: 0 0.5em;
 
-  &:hover,
-  &:focus {
-    border-color: transparent;
-    box-shadow: none;
-    outline-style: none;
-  }
-
-  &:hover {
-    --color-back: var(--color-primary);
-  }
-
-  &:focus {
-    --color-back: #aaa;
-  }
+  padding: var(--sub-grid-unit);
+  height: calc((var(--grid-unit) / 2) - (var(--border-width) * 2));
 
   &::placeholder {
-    color: #999;
-    font-family: var(--font-interface);
-    font-size: 1em;
-    font-style: italic;
+    color: var(--color-text-placeholder);
   }
 `
 
