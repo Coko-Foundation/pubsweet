93df7af6449769e822c882957a0be054c81c68b5
Samuel Galson <samgalson@gmail.com>
2018-02-14 12:19:43 +0000

    fix(components): styleguide can render components using validations

--- packages/components/webpack.config.js
+++ packages/components/webpack.config.js
@@ -1,3 +1,22 @@
-const webpackConfig = require('@pubsweet/styleguide/src/webpack-config')
+const path = require('path')
+const config = require('config')
+const fs = require('fs-extra')
 
-module.exports = webpackConfig(__dirname)
+// can't use node-config in webpack so save whitelisted client config into the build and alias it below
+const outputPath = path.resolve(__dirname, '..', '_build', 'config')
+fs.ensureDirSync(outputPath)
+const clientConfigPath = path.join(outputPath, 'client-config.json')
+fs.writeJsonSync(clientConfigPath, config, { spaces: 2 })
+
+const makeWebpackConfig = require('@pubsweet/styleguide/src/webpack-config')
+
+const webpackConfig = makeWebpackConfig(__dirname)
+
+webpackConfig.resolve = {
+  alias: {
+    joi: 'joi-browser',
+    config: clientConfigPath,
+  },
+}
+
+module.exports = webpackConfig
