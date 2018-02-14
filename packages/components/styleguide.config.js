d0366818908cee65a3495ab959616730d7f70db7
Samuel Galson <samgalson@gmail.com>
2018-02-14 10:29:42 +0000

    fix(components): make styleguide work (mostly)

--- packages/components/styleguide.config.js
+++ packages/components/styleguide.config.js
@@ -1,8 +1,17 @@
 module.exports = {
+  context: {
+    faker: 'faker',
+  },
   styleguideComponents: {
-    Wrapper: require.resolve('./styleguidist/Wrapper.jsx'),
+    Wrapper: require.resolve('@pubsweet/styleguide/src/components/Wrapper'),
+    StyleGuideRenderer: require.resolve(
+      '@pubsweet/styleguide/src/components/StyleGuideRenderer',
+    ),
   },
   components: 'packages/**/*.jsx',
+  skipComponentsWithoutExample: true,
+  serverPort: 6063,
+  title: 'PubSweet Components',
   ignore: [
     '**/packages/Manage/**',
     '**/packages/InkBackend/**',
