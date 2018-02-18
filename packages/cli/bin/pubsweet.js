7a92e1e6d74ac7cd00732d4cafec97393d8b81a4
Alex Theg <charles.theg@gmail.com>
2018-02-17 23:43:40 -0800

    feat(cli): show help when unknown command is used
    fix #107 #114

--- packages/cli/bin/pubsweet.js
+++ packages/cli/bin/pubsweet.js
@@ -13,4 +13,19 @@ program
   .command('remove', 'remove one or more components from a pubsweet app')
   .command('adduser', 'add a user to the database for a pubsweet app')
 
+// only runs if there are cmnds/opts entered
+if (process.argv.length > 2) {
+  const cmdArr = []
+  const keyArr = Object.keys(program.commands)
+  for (let i = 0; i < keyArr.length; i += i) {
+    cmdArr[i] = program.commands[keyArr[i]]._name
+  }
+
+  // show help if arg isn't a valid cmd or an option flag
+  const argListStart = process.argv[2]
+  if (!cmdArr.includes(argListStart) && argListStart[0] !== '-') {
+    program.help()
+  }
+}
+
 program.parse(process.argv)
