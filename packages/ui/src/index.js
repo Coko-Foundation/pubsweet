b9727befa10dc90700c41e7d62efb04f4f298c9d
Mihail Dunaev <mihail.dunaev91@gmail.com>
2018-03-07 12:12:42 +0000

    refactor(ui): fix imports in ui package
    use local imports instead of package/global ones and make the code a little bit more maintainable

--- packages/ui/src/index.js
+++ packages/ui/src/index.js
@@ -1,45 +1,6 @@
-/* atoms */
-export { default as Attachment } from './atoms/Attachment'
-export { default as Avatar } from './atoms/Avatar'
-export { default as Badge } from './atoms/Badge'
-export { default as Button } from './atoms/Button'
-export { default as Checkbox } from './atoms/Checkbox'
-export { default as File } from './atoms/File'
-export { default as Icon } from './atoms/Icon'
-export { default as Menu } from './atoms/Menu'
-export { default as Radio } from './atoms/Radio'
-export { default as Tags } from './atoms/Tags'
-export { default as Spinner } from './atoms/Spinner'
-export { default as TextField } from './atoms/TextField'
-export { default as ValidatedField } from './atoms/ValidatedField'
-export { default as StateItem } from './atoms/StateItem'
-export { default as AlignmentBox } from './atoms/AlignmentBox'
-export { default as UploadButton } from './atoms/UploadButton'
-export { default as Flexbox } from './atoms/Flexbox'
-export { default as Link } from './atoms/Link'
-export { default as Heading, H1, H2, H3, H4, H5, H6 } from './atoms/Heading'
-export { default as ErrorText } from './atoms/ErrorText'
-export { default as CenteredColumn } from './atoms/CenteredColumn'
-export { default as Section } from './atoms/Section'
-export { default as Colorize } from './atoms/Colorize'
-
-/* molecules */
-export { default as AppBar } from './molecules/AppBar'
-export { default as Attachments } from './molecules/Attachments'
-export { default as CheckboxGroup } from './molecules/CheckboxGroup'
-export { default as EditableValue } from './molecules/EditableValue'
-export { default as Files } from './molecules/Files'
-export { default as FileUploadList } from './molecules/FileUploadList'
-export { default as PlainButton } from './molecules/PlainButton'
-export { default as Supplementary } from './molecules/Supplementary'
-export { default as RadioGroup } from './molecules/RadioGroup'
-export { default as YesOrNo } from './molecules/YesOrNo'
-export { default as StateList } from './molecules/StateList'
-export {
-  default as AlignmentBoxWithLabel,
-} from './molecules/AlignmentBoxWithLabel'
-export { default as AlignmentTool } from './molecules/AlignmentTool'
-export { default as Steps } from './molecules/Steps'
+/* wrapper for atoms/index.js and molecules/index.js */
+export * from './atoms'
+export * from './molecules'
 
 /* helpers */
 export { default as th } from './helpers/themeHelper'
