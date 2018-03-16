d0be1d82dfb6810e7bc5ec8891fe3848b58c24f4
Sebastian <sebastian.mihalache@thinslices.com>
2018-03-16 10:41:17 +0200

    fix(component-send-email): remove sendMail callback

--- packages/components/packages/SendEmail/src/SendEmail.js
+++ packages/components/packages/SendEmail/src/SendEmail.js
@@ -6,11 +6,6 @@ const mailerConfig = require(config.mailer.path)
 module.exports = {
   send: mailData => {
     const transporter = nodemailer.createTransport(mailerConfig.transport)
-    return transporter.sendMail(mailData, (err, info) => {
-      if (err) {
-        logger.error(err)
-      }
-      logger.info(`success message id: ${info.response}`)
-    })
+    return transporter.sendMail(mailData)
   },
 }
