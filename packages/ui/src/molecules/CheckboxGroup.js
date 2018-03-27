a58e0d64301fbd76c41ff180153125a0f8056606
Jure Triglav <juretriglav@gmail.com>
2018-03-27 18:01:31 +0200

    fix(ui): update CheckboxGroup rendering if props change

--- packages/ui/src/molecules/CheckboxGroup.js
+++ packages/ui/src/molecules/CheckboxGroup.js
@@ -3,16 +3,8 @@ import React from 'react'
 import { Checkbox, Flexbox } from '../atoms'
 
 class CheckboxGroup extends React.Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      values: props.value || [],
-    }
-  }
-
   handleChange = event => {
-    const values = [...this.state.values]
+    const values = this.props.value ? Array.from(this.props.value) : []
 
     const { value } = event.target
 
@@ -22,20 +14,17 @@ class CheckboxGroup extends React.Component {
       values.splice(values.indexOf(value), 1)
     }
 
-    this.setState({ values })
-
     this.props.onChange(values)
   }
 
   render() {
-    const { inline, name, options, required } = this.props
-    const { values } = this.state
+    const { value, inline, name, options, required } = this.props
 
     return (
       <Flexbox column={!inline}>
         {options.map(option => (
           <Checkbox
-            checked={values.includes(option.value)}
+            checked={value.includes(option.value)}
             inline={inline}
             key={option.value}
             label={option.label}
