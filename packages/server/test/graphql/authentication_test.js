6432b58a93a8ce02fff501ba69dd36cf04be0d58
Tamlyn Rhodes <tamlyn@tamlyn.org>
2018-04-12 10:46:27 +0100

    feat(server): simplify currentUser query response
    Return `null` when unauthenticated instead of throwing error.
    BREAKING CHANGE: remove `token` from `currentUser` query response since it must have been provided in the request.

--- packages/server/test/graphql/authentication_test.js
+++ packages/server/test/graphql/authentication_test.js
@@ -15,102 +15,144 @@ describe('GraphQL authentication', () => {
     token = authentication.token.create(user)
   })
 
-  it('can log in', async () => {
-    const { body } = await api.graphql.query(
-      `mutation($input: LoginUserInput) {
+  describe('loginUser mutation', () => {
+    it('can log in', async () => {
+      const { body } = await api.graphql.query(
+        `mutation($input: LoginUserInput) {
           loginUser(input: $input) {
             user { username }
             token
           }
         }`,
-      { input: { username: 'admin', password: 'admin' } },
-    )
+        { input: { username: 'admin', password: 'admin' } },
+      )
 
-    expect(body).toMatchObject({
-      data: {
-        loginUser: { token: expect.any(String), user: { username: 'admin' } },
-      },
+      expect(body).toMatchObject({
+        data: {
+          loginUser: { token: expect.any(String), user: { username: 'admin' } },
+        },
+      })
     })
-  })
 
-  it('blocks invalid login', async () => {
-    const { body } = await api.graphql.query(
-      `mutation($input: LoginUserInput) {
+    it('blocks invalid login', async () => {
+      const { body } = await api.graphql.query(
+        `mutation($input: LoginUserInput) {
           loginUser(input: $input) {
             token
           }
         }`,
-      { input: { username: 'admin', password: 'not correct' } },
-    )
+        { input: { username: 'admin', password: 'not correct' } },
+      )
 
-    expect(body).toMatchObject({
-      data: { loginUser: null },
-      errors: [{ message: 'Wrong username or password.' }],
+      expect(body).toMatchObject({
+        data: { loginUser: null },
+        errors: [{ message: 'Wrong username or password.' }],
+      })
     })
   })
 
-  it('fetches current user from token', async () => {
-    const { body } = await api.graphql.query(
-      `{ currentUser { user { username, email } token} }`,
-      {},
-      token,
-    )
+  describe('currentUser query', () => {
+    it('returns null when unauthenticated', async () => {
+      const { body } = await api.graphql.query(
+        `{ currentUser { username, email} }`,
+      )
 
-    expect(body).toMatchObject({
-      data: {
-        currentUser: {
-          user: { username: 'admin', email: 'admin@admins.example.org' },
-          token: expect.any(String),
+      expect(body).toMatchObject({
+        data: {
+          currentUser: null,
         },
-      },
+      })
+    })
+
+    it('fetches current user from token', async () => {
+      const { body } = await api.graphql.query(
+        `{ currentUser { username, email} }`,
+        {},
+        token,
+      )
+
+      expect(body).toMatchObject({
+        data: {
+          currentUser: {
+            username: 'admin',
+            email: 'admin@admins.example.org',
+          },
+        },
+      })
+    })
+
+    it('errors when user not found', async () => {
+      const badToken = authentication.token.create({
+        id: '123e4567-e89b-12d3-a456-426655440000',
+        username: 'does not exist',
+      })
+      const { body } = await api.graphql.query(
+        `{ currentUser { username, email} }`,
+        {},
+        badToken,
+      )
+
+      expect(body).toMatchObject({
+        data: {
+          currentUser: null,
+        },
+        errors: [
+          {
+            message:
+              'Object not found: user with id 123e4567-e89b-12d3-a456-426655440000',
+          },
+        ],
+      })
     })
   })
 
-  it('errors when unauthenticated', async () => {
-    const { body } = await api.graphql.query(`{ users { username } }`)
+  describe('user query', () => {
+    it('errors when unauthenticated', async () => {
+      const { body } = await api.graphql.query(`{ users { username } }`)
 
-    expect(body).toMatchObject({
-      data: { users: null },
-      errors: [{ message: 'Operation not permitted: read users' }],
+      expect(body).toMatchObject({
+        data: { users: null },
+        errors: [{ message: 'Operation not permitted: read users' }],
+      })
     })
-  })
 
-  it('filters the returned data', async () => {
-    jest
-      .spyOn(authsome, 'can')
-      .mockReturnValue({ filter: user => omit(user, 'admin') })
+    it('filters the returned data', async () => {
+      jest
+        .spyOn(authsome, 'can')
+        .mockReturnValue({ filter: user => omit(user, 'admin') })
 
-    const { body } = await api.graphql.query(
-      `{ users { username, admin } }`,
-      {},
-      token,
-    )
+      const { body } = await api.graphql.query(
+        `{ users { username, admin } }`,
+        {},
+        token,
+      )
 
-    expect(body).toEqual({
-      data: { users: [{ username: 'admin', admin: null }] },
+      expect(body).toEqual({
+        data: { users: [{ username: 'admin', admin: null }] },
+      })
     })
-  })
 
-  it('returns not found if not authorized', async () => {
-    jest
-      .spyOn(authsome, 'can')
-      .mockReturnValueOnce(true)
-      .mockReturnValueOnce(false)
+    it('returns not found if not authorized', async () => {
+      jest
+        .spyOn(authsome, 'can')
+        .mockReturnValueOnce(true)
+        .mockReturnValueOnce(false)
 
-    const { body } = await api.graphql.query(
-      `query($id: ID) {
+      const { body } = await api.graphql.query(
+        `query($id: ID) {
           user(id: $id) { 
             username
             admin 
           }
         }`,
-      { id: user.id },
-      token,
-    )
+        { id: user.id },
+        token,
+      )
 
-    expect(body).toMatchObject({
-      data: { user: null },
-      errors: [{ message: `Object not found: user with id ${user.id}` }],
+      expect(body).toMatchObject({
+        data: { user: null },
+        errors: [{ message: `Object not found: user with id ${user.id}` }],
+      })
     })
   })
 })
