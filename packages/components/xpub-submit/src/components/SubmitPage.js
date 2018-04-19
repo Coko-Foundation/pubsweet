cfb2a817f000a63a2994140df3b3a814d534b26b
Jure Triglav <juretriglav@gmail.com>
2018-04-19 14:24:57 +0200

    feat(xpub-submit): move GraphQL functionality into separate component

--- packages/components/xpub-submit/src/components/SubmitPage.js
+++ packages/components/xpub-submit/src/components/SubmitPage.js
@@ -1,111 +1,41 @@
-import { pick, throttle, defaultsDeep } from 'lodash'
+import { pick, throttle } from 'lodash'
 import { compose, withProps, withState, withHandlers } from 'recompose'
-import { graphql } from 'react-apollo'
-import { gql } from 'apollo-client-preset'
+import { connect } from 'react-redux'
 import { reduxForm, SubmissionError } from 'redux-form'
-import { withLoader } from 'pubsweet-client'
-import uploadFile from 'xpub-upload/src/no-redux'
+import { actions } from 'pubsweet-client'
+import uploadFile from 'xpub-upload'
+import { ConnectPage } from 'xpub-connect'
+import { selectCollection, selectFragment } from 'xpub-selectors'
 import Submit from './Submit'
 
-const fragmentFields = `
-  id
-  metadata {
-    title
-    abstract
-    authors
-    keywords
-    articleType
-    articleSection
-  }
-  declarations {
-    openData
-    previouslySubmitted
-    openPeerReview
-    streamlinedReview
-    researchNexus
-    preregistered
-  }
-  suggestions {
-    reviewers {
-      suggested
-      opposed
-    }
-    editors {
-      suggested
-      opposed
-    }
-  }
-  notes {
-    fundingAcknowledgement
-    specialInstructions
-  }
-  files {
-    manuscript {
-      name
-      type
-      size
-      url
-    }
-    supplementary {
-      name
-      type
-      size
-      url
-    }
-  }
-`
+const onSubmit = (values, dispatch, { history, project, version }) =>
+  // console.log('submit', values)
 
-const query = gql`
-  query($id: ID) {
-    collection(id: $id) {
-      id
-      fragments {
-        ${fragmentFields}
+  dispatch(
+    actions.updateFragment(project, {
+      id: version.id,
+      rev: version.rev,
+      submitted: new Date(),
+      ...values,
+    }),
+  )
+    .then(() =>
+      dispatch(
+        actions.updateCollection({
+          id: project.id,
+          rev: project.rev,
+          status: 'submitted',
+        }),
+      ),
+    )
+    .then(() => {
+      history.push('/')
+    })
+    .catch(error => {
+      if (error.validationErrors) {
+        throw new SubmissionError()
       }
-    }
-    currentUser {
-      user {
-        id
-        username
-        admin
-      }
-    }
-  }
-`
-
-const updateMutation = gql`
-  mutation($id: ID, $input: String) {
-    updateFragment(id: $id, input: $input) {
-      id
-      ${fragmentFields}
-    }
-  }
-`
-
-const submitMutation = gql`
-  mutation(
-    $fragmentId: ID
-    $fragmentInput: String
-    $collectionId: ID
-    $collectionInput: String
-  ) {
-    updateFragment(id: $fragmentId, input: $fragmentInput) {
-      id
-      submitted
-    }
-
-    updateCollection(id: $collectionId, input: $collectionInput) {
-      id
-      status
-    }
-  }
-`
-
-// TODO: deep-clone values instead of mutating it?
-const prepareValuesForUpdate = values => {
-  values.metadata.title = stripHtml(values.metadata.title)
-  return values
-}
+    })
 
 // TODO: this is only here because prosemirror would save the title in the
 // metadata as html instead of plain text. we need to maybe find a better
@@ -116,135 +46,54 @@ const stripHtml = htmlString => {
   return temp.textContent
 }
 
-const omitSpecialKeysDeep = object => {
-  const output = {}
+// TODO: redux-form doesn't have an onBlur handler(?)
+const onChange = (values, dispatch, { project, version }) => {
+  values.metadata.title = stripHtml(values.metadata.title) // see TODO above
 
-  Object.entries(object).forEach(([key, value]) => {
-    if (!key.startsWith('_')) {
-      if (
-        typeof value === 'object' &&
-        value !== null &&
-        !Array.isArray(value)
-      ) {
-        output[key] = omitSpecialKeysDeep(value)
-      } else {
-        output[key] = value
-      }
-    }
-  })
+  return dispatch(
+    actions.updateFragment(project, {
+      id: version.id,
+      rev: version.rev,
+      // submitted: false,
+      ...values,
+    }),
+  )
 
-  return output
-}
-
-const stripNullsDeep = object => {
-  const output = {}
-
-  Object.entries(object).forEach(([key, value]) => {
-    if (value !== null) {
-      if (typeof value === 'object' && !Array.isArray(value)) {
-        output[key] = stripNullsDeep(value)
-      } else {
-        output[key] = value
-      }
-    }
-  })
-
-  return output
+  // TODO: display a notification when saving/saving completes/saving fails
 }
 
 export default compose(
-  graphql(query, {
-    options: ({ match }) => ({
-      variables: {
-        id: match.params.project,
-      },
-    }),
-  }),
-  graphql(updateMutation, {
-    props: ({ mutate, ownProps }) => {
-      const updateFragment = rawValues => {
-        const values = prepareValuesForUpdate(rawValues)
+  ConnectPage(({ match }) => [
+    actions.getCollection({ id: match.params.project }),
+    actions.getFragment(
+      { id: match.params.project },
+      { id: match.params.version },
+    ),
+  ]),
+  connect(
+    (state, { match }) => {
+      const project = selectCollection(state, match.params.project)
+      const version = selectFragment(state, match.params.version)
 
-        mutate({
-          variables: {
-            id: ownProps.match.params.version,
-            input: JSON.stringify(values),
-          },
-        })
-      }
-
-      return {
-        // TODO: do this on blur, rather than on every keystroke?
-        onChange: throttle(updateFragment, 3000, { trailing: false }),
-      }
+      return { project, version }
     },
-  }),
-  graphql(submitMutation, {
-    props: ({ mutate }) => ({
-      onSubmit: (rawValues, dispatch, { history, project, version }) => {
-        const values = prepareValuesForUpdate({
-          ...rawValues,
-          submitted: new Date(),
-        })
-
-        mutate({
-          variables: {
-            fragmentId: version.id,
-            fragmentInput: JSON.stringify(values),
-            collectionId: project.id,
-            collectionInput: JSON.stringify({ status: 'submitted' }),
-          },
-        })
-          .then(() => {
-            history.push('/')
-          })
-          .catch(error => {
-            if (error.validationErrors) {
-              throw new SubmissionError()
-            }
-          })
-      },
-    }),
-  }),
-  withLoader(),
-  withProps(({ collection, currentUser }) => ({
-    project: collection,
-    version: collection.fragments[0],
-    uploadFile,
-  })),
+    {
+      uploadFile,
+    },
+  ),
   withProps(({ version }) => {
     const paths = ['metadata', 'declarations', 'suggestions', 'notes', 'files']
-    const initialValues = defaultsDeep(
-      stripNullsDeep(omitSpecialKeysDeep(pick(version, paths))),
-      {
-        metadata: {
-          title: '',
-          abstract: '',
-          articleType: '',
-          articleSection: [],
-          authors: [],
-          keywords: [],
-        },
-        suggestions: {
-          reviewers: {
-            suggested: [],
-            opposed: [],
-          },
-          editors: {
-            suggested: [],
-            opposed: [],
-          },
-        },
-      },
-    )
+
     return {
-      initialValues,
+      initialValues: pick(version, paths),
       readonly: !!version.submitted,
     }
   }),
   reduxForm({
     // enableReinitialize: true,
     form: 'submit',
+    onChange: throttle(onChange, 3000, { trailing: false }),
+    onSubmit,
   }),
   withState('confirming', 'setConfirming', false),
   withHandlers({
