c7fb15d86ee4aa0c01f90c43e8730d0c7e82f463
ana <ana@lynx.nz>
2018-04-22 13:50:05 +1200

    add and remove author buttons

--- packages/components/xpub-submit/src/components/AuthorsInput.js
+++ packages/components/xpub-submit/src/components/AuthorsInput.js
@@ -1,70 +1,103 @@
 import React from 'react'
 import { FieldArray } from 'redux-form'
 import { TextField, ValidatedField, Button } from '@pubsweet/ui'
-import {
-  required,
-  minSize,
-} from 'xpub-validators'
+import { required, minSize } from 'xpub-validators'
 
 const minSize1 = minSize(1)
 
-const renderField = ({ input, label, placeholder }) => {
-  <div>
-    <TextField placeholder={placeholder} label={label} {...input} />
+const firstNameInput = ({ input }) => {
+  ;<div>
+    <TextField
+      placeholder={'Enter first name…'}
+      label={'First name'}
+      {...input}
+    />
   </div>
 }
-//{/* TODO validation */}
+
+const lastNameInput = ({ input }) => {
+  ;<div>
+    <TextField
+      placeholder={'Enter last name…'}
+      label={'Last name'}
+      {...input}
+    />
+  </div>
+}
+
+const emailAddressInput = ({ input }) => {
+  ;<div>
+    <TextField
+      placeholder={'Enter email address…'}
+      label={'Email address'}
+      {...input}
+    />
+  </div>
+}
+
+const affiliationInput = ({ input }) => {
+  ;<div>
+    <TextField
+      placeholder={'Enter affiliation…'}
+      label={'Affiliation'}
+      {...input}
+    />
+  </div>
+}
+
 const renderAuthors = ({ authors }) => {
   <ul>
     <li>
-      {/* TODO Add another author button <Button> component */}
-      {/* TODO Remove author button When at least one remaining */}
+      <Button onClick={() => authors.push()} plain type="button">
+        Add another author
+      </Button>
     </li>
-    { authors.map((author, index) =>
-        <li key={index}>
-          <div>Author #{index + 1}</div>
+    {authors.map((author, index) => (
+      <li key={index}>
+        <Button onClick={() => authors.remove(index)} plain type="button">
+          Remove this author
+        </Button>
 
-          <ValidatedField
-            component={renderField} //label="First name", placeholder="Enter first name…"
-            name={`${author}.firstName`}
-            readonly={readonly}
-            required
-            validate={[minSize1]}    
-          />
+        <div>Author #{index + 1}</div>
 
-          <ValidatedField 
-            component={renderField} //label="Last name", placeholder="Enter last name…"
-            name={`${author}.lastName`}
-            readonly={readonly}
-            required
-            validate={[minSize1]}    
-          />
+        <ValidatedField
+          component={firstNameInput}
+          name={`${author}.firstName`}
+          readonly={readonly}
+          required
+          validate={[minSize1]}
+        />
 
-          <ValidatedField 
-            component={renderField} //label="Email address", placeholder="Enter email address…"
-            name={`${author}.email`}
-            readonly={readonly}
-            required
-            validate={[minSize1]}    
-          />
+        <ValidatedField
+          component={lastNameInput}
+          name={`${author}.lastName`}
+          readonly={readonly}
+          required
+          validate={[minSize1]}
+        />
 
-          <ValidatedField 
-            component={renderField} //label="Affiliation", placeholder="Enter affiliation…"
-            name={`${author}.affiliation`}
-            readonly={readonly}
-            required
-            validate={[minSize1]}  
-          />
-        </li>
-        )
-      }
-  </ul>  
+        <ValidatedField
+          component={emailAddressInput}
+          name={`${author}.email`}
+          readonly={readonly}
+          required
+          validate={[minSize1]}
+        />
+
+        <ValidatedField
+          component={affiliationInput}
+          name={`${author}.affiliation`}
+          readonly={readonly}
+          required
+          validate={[minSize1]}
+        />
+      </li>
+    ))}
+  </ul>
 }
 
 const AuthorsInput = () => {
-  return (
-    <FieldArray name="authors" component={renderAuthors} />
-  )
+  return <FieldArray name="authors" component={renderAuthors} />
 }
 
 export default AuthorsInput
