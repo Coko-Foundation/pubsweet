be49be54be6b7cf7a2fd030bfce8d885baabed3a
Tamlyn Rhodes <tamlyn@tamlyn.org>
2018-04-23 16:24:01 +0100

    feat: migration runner
    Load migration files from pubsweet-server, app and components, copy them to a temporary folder and run them in alphabetical order.

--- packages/db-manager/src/commands/create-tables.js
+++ packages/db-manager/src/commands/create-tables.js
@@ -1,18 +1,36 @@
 const logger = require('@pubsweet/logger')
 const db = require('pubsweet-server/src/db')
+const Umzug = require('umzug')
+const fs = require('fs-extra')
+const path = require('path')
+const tmp = require('tmp')
+const { promisify } = require('util')
+const storage = require('../helpers/umzugStorage')
+const getMigrationPaths = require('../helpers/migrationPaths')
 
-module.exports = async clobber => {
+const makeTempDir = promisify(tmp.dir)
+
+// load SQL files as migrations
+const sqlResolver = filePath => ({
+  up: async db => {
+    const fileContents = await fs.readFile(filePath, 'utf-8')
+    return db.query(fileContents)
+  },
+})
+
+const createTables = async clobber => {
   const { rows } = await db.query(`
-    SELECT table_name
-    FROM information_schema.tables
-    WHERE table_schema='public'
+    SELECT tablename
+    FROM pg_tables
+    WHERE schemaname = current_schema
   `)
 
   if (rows.length) {
     if (clobber) {
       logger.info('Overwriting existing database due to clobber option')
       await Promise.all(
-        rows.map(row => db.query(`DROP TABLE ${row.table_name}`)),
+        // TODO this is dangerous, change it
+        rows.map(row => db.query(`DROP TABLE ${row.tablename}`)),
       )
     } else {
       logger.error(
@@ -22,5 +40,35 @@ module.exports = async clobber => {
     }
   }
 
-  await db.query('CREATE TABLE entities (id UUID PRIMARY KEY, data JSONB)')
+  // collect up all migrations to be run
+  const tempDir = await makeTempDir({
+    prefix: 'migrations-',
+    unsafeCleanup: true,
+  })
+
+  const migrationPaths = getMigrationPaths()
+  await Promise.all(
+    migrationPaths.map(migrationPath => fs.copy(migrationPath, tempDir)),
+  )
+
+  const migrator = new Umzug({
+    storage,
+    logging: logger.debug.bind(logger),
+    migrations: {
+      path: tempDir,
+      params: [db],
+      pattern: /\d+-\w+\.(js|sql)/,
+      customResolver: filePath => {
+        if (path.extname(filePath) === '.sql') {
+          return sqlResolver(filePath)
+        }
+        return require(filePath)
+      },
+    },
+  })
+
+  // run migrations
+  await migrator.up()
 }
+
+module.exports = createTables
---
+++ packages/db-manager/src/helpers/umzugStorage.js
@@ -0,0 +1,23 @@
+const db = require('pubsweet-server/src/db')
+
+// umzug storage adapter
+module.exports = {
+  async logMigration(id) {
+    await db.query('INSERT INTO migrations (id) VALUES ($1)', [id])
+  },
+
+  async unlogMigration(id) {
+    await db.query('DELETE FROM migrations WHERE id = $1', [id])
+  },
+
+  async executed() {
+    await db.query(
+      `CREATE TABLE IF NOT EXISTS migrations (
+        id TEXT PRIMARY KEY,
+        run_at TIMESTAMPTZ DEFAULT current_timestamp
+      )`,
+    )
+    const { rows } = await db.query('SELECT id FROM migrations')
+    return rows
+  },
+}
