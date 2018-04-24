20c282c6a881a85600b5771458fd579e1711ace0
Giannis Kopanas <jkopanas@gmail.com>
2018-04-24 17:13:48 +0300

    fix(components): fix unsued lint error

--- packages/components/xpub-review/src/components/metadata/ReviewMetadata.js
+++ packages/components/xpub-review/src/components/metadata/ReviewMetadata.js
@@ -2,7 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 import { withJournal } from 'xpub-journal'
 
-import { File, Attachment } from '@pubsweet/ui'
+import { Attachment } from '@pubsweet/ui'
 
 const Root = styled.div``
 
