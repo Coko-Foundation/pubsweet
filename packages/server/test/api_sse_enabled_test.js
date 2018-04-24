be67ddec1d963636fa6670cc7aed29773fb90bbb
Jure Triglav <juretriglav@gmail.com>
2018-04-24 13:22:12 +0200

    test(sse): add more filtering tests for SSE

--- packages/server/test/api_sse_enabled_test.js
+++ packages/server/test/api_sse_enabled_test.js
@@ -64,84 +64,99 @@ describe('API SSE enabled', () => {
     })
   })
 
-  describe('event filter', () => {
-    it('supports not sending an event', async () => {
-      const adminToken = await api.users.authenticate.post(fixtures.adminUser)
-      const token = await api.users.authenticate.post(fixtures.user)
+  it('supports not sending an event', async () => {
+    const adminToken = await api.users.authenticate.post(fixtures.adminUser)
+    const token = await api.users.authenticate.post(fixtures.user)
 
-      es = new EventSource(
-        `http://localhost:${port}/updates?access_token=${encodeURIComponent(
-          token,
-        )}`,
-      )
+    es = new EventSource(
+      `http://localhost:${port}/updates?access_token=${encodeURIComponent(
+        token,
+      )}`,
+    )
 
-      // wrap user's event listener in promise
-      const eventPromise = new Promise(resolve =>
-        es.addEventListener('message', resolve),
-      )
+    // wrap user's event listener in promise
+    const eventPromise = new Promise(resolve =>
+      es.addEventListener('message', resolve),
+    )
 
-      // perform action (we'll block the SSE for this one)
-      await api.fragments
-        .post({ fragment: fixtures.fragment, token: adminToken })
-        .expect(STATUS.CREATED)
+    // perform action (we'll block the SSE for this one)
+    await api.fragments
+      .post({ fragment: fixtures.fragment, token: adminToken })
+      .expect(STATUS.CREATED)
 
-      // perform action (let this one through filtered)
-      await api.collections
-        .create(fixtures.collection, adminToken)
-        .expect(STATUS.CREATED)
+    // perform action (let this one through filtered)
+    await api.collections
+      .create(fixtures.collection, adminToken)
+      .expect(STATUS.CREATED)
 
-      // await user's filtered event
-      const event = await eventPromise
-      const eventData = JSON.parse(event.data)
+    // await user's filtered event
+    const event = await eventPromise
+    const eventData = JSON.parse(event.data)
 
-      expect(eventData).toEqual(
-        expect.objectContaining({
-          action: 'collection:create',
-          data: {
-            collection: {
-              id: expect.any(String),
-              title: fixtures.collection.title,
-            },
+    expect(eventData).toEqual(
+      expect.objectContaining({
+        action: 'collection:create',
+        data: {
+          collection: {
+            id: expect.any(String),
+            title: fixtures.collection.title,
           },
-        }),
-      )
-    })
+        },
+      }),
+    )
+  })
 
-    it('supports property-filtering', async () => {
-      const adminToken = await api.users.authenticate.post(fixtures.adminUser)
-      const token = await api.users.authenticate.post(fixtures.user)
+  it('supports property-filtering', async () => {
+    const adminToken = await api.users.authenticate.post(fixtures.adminUser)
+    const token = await api.users.authenticate.post(fixtures.user)
 
-      es = new EventSource(
-        `http://localhost:${port}/updates?access_token=${encodeURIComponent(
-          token,
-        )}`,
-      )
+    adminEs = new EventSource(
+      `http://localhost:${port}/updates?access_token=${encodeURIComponent(
+        adminToken,
+      )}`,
+    )
 
-      // wrap user's event listener in promise
-      const eventPromise = new Promise(resolve =>
-        es.addEventListener('message', resolve),
-      )
+    es = new EventSource(
+      `http://localhost:${port}/updates?access_token=${encodeURIComponent(
+        token,
+      )}`,
+    )
 
-      // perform action
-      await api.collections
-        .create(fixtures.collection, adminToken)
-        .expect(STATUS.CREATED)
+    const adminEventPromise = new Promise(resolve =>
+      adminEs.addEventListener('message', resolve),
+    )
 
-      // await user's filtered event
-      const event = await eventPromise
-      const eventData = JSON.parse(event.data)
+    const eventPromise = new Promise(resolve =>
+      es.addEventListener('message', resolve),
+    )
 
-      expect(eventData).toEqual(
-        expect.objectContaining({
-          action: 'collection:create',
-          data: {
-            collection: {
-              id: expect.any(String),
-              title: fixtures.collection.title,
-            },
+    // perform action
+    await api.collections
+      .create(fixtures.collection, adminToken)
+      .expect(STATUS.CREATED)
+
+    // await admins unfiltered event
+    const adminEvent = await adminEventPromise
+    const adminEventData = JSON.parse(adminEvent.data)
+
+    expect(Object.keys(adminEventData.data.collection)).toEqual(
+      expect.arrayContaining(['id', 'created', 'title', 'owners']),
+    )
+
+    // await user's filtered event
+    const event = await eventPromise
+    const eventData = JSON.parse(event.data)
+
+    expect(eventData).toEqual(
+      expect.objectContaining({
+        action: 'collection:create',
+        data: {
+          collection: {
+            id: expect.any(String),
+            title: fixtures.collection.title,
           },
-        }),
-      )
-    })
+        },
+      }),
+    )
   })
 })
