dd62163bb88a227693abf78c6d58222d8a077547
Yannis Barlas <yannisbarlas@gmail.com>
2018-04-29 01:25:52 +0300

    refactor(ui): rewrite button in a less repetitive way

--- packages/ui/src/atoms/Button.js
+++ packages/ui/src/atoms/Button.js
@@ -1,17 +1,39 @@
 // import React from 'react'
 import { get } from 'lodash'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 import th from '../helpers/themeHelper'
+
+const primary = css`
+  background: ${th('colorPrimary')};
+  color: ${th('colorTextReverse')};
+
+  &:focus,
+  &:hover {
+    // DARKEN 30
+    background-color: #16415d;
+  }
+
+  &:active {
+    // DARKEN 50
+    background-color: #102e43;
+  }
+
+  &[disabled] {
+    &:focus,
+    &:hover,
+    &:active {
+      background: ${th('colorPrimary')};
+    }
+  }
+`
 
 const StyledButton = styled.button.attrs({
   type: 'button',
 })`
-  background: ${props =>
-    props.primary ? th('colorPrimary') : th('colorSecondary')};
-
+  background: ${th('colorSecondary')};
   border: ${th('borderWidth')} ${th('borderStyle')} ${th('colorBorder')};
   border-radius: ${th('borderRadius')};
-  color: ${props => (props.primary ? th('colorTextReverse') : th('colorText'))}
+  color: ${th('colorText')};
   font-family: ${th('fontInterface')};
   font-size: ${th('fontSizeBase')};
   line-height: ${th('gridUnit')};
@@ -21,13 +43,13 @@ const StyledButton = styled.button.attrs({
   &:focus,
   &:hover {
     // DARKEN 30
-    background-color: ${props => (props.primary ? '#16415D' : '#A1A1A1')};
+    background-color: #a1a1a1;
     transition: ${th('transitionDuration')} ${th('transitionTimingFunction')};
   }
 
   &:active {
     // DARKEN 50
-    background-color: ${props => (props.primary ? '#102E43' : '#737373')};
+    background-color: '#737373';
   }
 
   &[disabled] {
@@ -37,12 +59,13 @@ const StyledButton = styled.button.attrs({
     &:focus,
     &:hover,
     &:active {
-      background: ${props =>
-        props.primary ? th('colorPrimary') : th('colorSecondary')};
+      background: ${th('colorSecondary')};
     }
   }
 
-  ${props => get(props.theme, 'cssOverrides.Button')}
+  ${props => props.primary && primary} ${props =>
+    get(props.theme, 'cssOverrides.Button')};
+  ${props => get(props.theme, 'cssOverrides.Button.Root')};
 `
 
 export default StyledButton
