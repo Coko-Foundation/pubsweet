db27c386fa314f9d06cd1d747dda18709654c5ce
Alexandros Georgantas <alexgeorg86@gmail.com>
2018-05-17 17:12:17 +0300

    test(team reducer): tests added and refactoring of reducer

--- packages/client/src/reducers/teams.js
+++ packages/client/src/reducers/teams.js
@@ -1,7 +1,8 @@
 import clone from 'lodash/clone'
 import findIndex from 'lodash/findIndex'
-import differenceBy from 'lodash/differenceBy'
+import find from 'lodash/find'
 import unionBy from 'lodash/unionBy'
+import without from 'lodash/without'
 
 import {
   GET_TEAMS_SUCCESS,
@@ -15,25 +16,39 @@ import {
 export default function(state = [], action) {
   const teams = clone(state)
 
-  function updateOne() {
-    const index = findIndex(teams, { id: action.team.id })
-    if (index !== -1) {
-      teams[index] = { ...teams[index], ...action.team }
-    } else {
+  function getTeam() {
+    return find(teams, { id: action.team.id })
+  }
+  function getTeamIndex() {
+    return findIndex(teams, { id: action.team.id })
+  }
+
+  function addTeam() {
+    if (!getTeam()) {
       teams.push(action.team)
     }
-
     return teams
   }
 
+  function updateOne() {
+    const index = getTeamIndex()
+    teams[index] = { ...teams[index], ...action.update }
+    return teams
+  }
+  function deleteTeam() {
+    const team = getTeam()
+
+    return without(teams, team)
+  }
   switch (action.type) {
     case CREATE_TEAM_SUCCESS:
+      return addTeam()
     case UPDATE_TEAM_SUCCESS:
       return updateOne()
     case GET_TEAMS_SUCCESS:
       return clone(action.teams)
     case DELETE_TEAM_SUCCESS:
-      return differenceBy(state, [action.team], 'id')
+      return deleteTeam()
     case LOGOUT_SUCCESS:
       return []
     case GET_COLLECTION_TEAMS_SUCCESS:
