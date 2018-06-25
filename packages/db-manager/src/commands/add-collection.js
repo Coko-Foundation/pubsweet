f17a49470d926fd637a92f47a9f20e8fab0bbd03
ana <ana@lynx.nz>
2018-06-25 22:04:55 +1200

    feat: remove line that adds first fragment to collection

--- packages/db-manager/src/commands/add-collection.js
+++ packages/db-manager/src/commands/add-collection.js
@@ -1,14 +1,12 @@
 const logger = require('@pubsweet/logger')
 const Collection = require('pubsweet-server/src/models/Collection')
 const User = require('pubsweet-server/src/models/User')
-const Fragment = require('pubsweet-server/src/models/Fragment')
 
-module.exports = async collectionData => {
+module.exports = async (collectionData, fragment = null) => {
   logger.info('Creating collection')
 
   const collection = await new Collection(collectionData).save()
   const [user] = await User.all()
-  const [fragment] = await Fragment.all()
 
   if (user) collection.setOwners([user.id])
   await collection.save()
