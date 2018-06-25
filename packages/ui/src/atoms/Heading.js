f24530aba4602710e63845ed2bf8ddef90466784
Samuel Galson <samgalson@gmail.com>
2018-06-25 16:35:56 +0100

    feat: make headings more reusable
    BREAKING CHANGE:
      * remove margins

--- packages/ui/src/atoms/Heading.js
+++ packages/ui/src/atoms/Heading.js
@@ -1,32 +1,38 @@
 import React from 'react'
 import styled, { css } from 'styled-components'
 import invariant from 'invariant'
-import { th } from '@pubsweet/ui-toolkit'
+import { th, override } from '@pubsweet/ui-toolkit'
 
 const style = css`
   color: ${th('colorPrimary')};
   font-size: ${({ level = 1, theme }) => theme[`fontSizeHeading${level}`]};
   font-family: ${th('fontHeading')};
-  margin: 0 0 ${th('gridUnit')};
+  ${override('Heading')};
 `
 
 export const H1 = styled.h1.attrs({ level: 1 })`
   ${style};
+  ${override('H1')};
 `
 export const H2 = styled.h2.attrs({ level: 2 })`
   ${style};
+  ${override('H2')};
 `
 export const H3 = styled.h3.attrs({ level: 3 })`
   ${style};
+  ${override('H3')};
 `
 export const H4 = styled.h4.attrs({ level: 4 })`
   ${style};
+  ${override('H4')};
 `
 export const H5 = styled.h5.attrs({ level: 5 })`
   ${style};
+  ${override('H5')};
 `
 export const H6 = styled.h6.attrs({ level: 6 })`
   ${style};
+  ${override('H6')};
 `
 
 const componentMap = [null, H1, H2, H3, H4, H5, H6]
