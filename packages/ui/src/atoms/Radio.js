874e93d8202ba7e4d2b11f0c83b2b480089a3244
Audrey Hamelers <hamelers@ebi.ac.uk>
2018-06-28 12:54:03 +0100

    refactor(ui): remove coko theme styles from vanilla ui
    Checkbox and Radio are not "vanilla" as implemented; theme styles need to be moved to coko-theme

--- packages/ui/src/atoms/Radio.js
+++ packages/ui/src/atoms/Radio.js
@@ -1,109 +1,26 @@
 import React from 'react'
-import styled, { keyframes } from 'styled-components'
+import styled from 'styled-components'
 import { th, override } from '@pubsweet/ui-toolkit'
 
-const Input = styled.input`
-  position: absolute;
-  opacity: 0;
-  z-index: -1;
-`
-
-const PseudoInput = styled.span`
-  display: inline-block;
-  content: ' ';
-  width: calc(${th('gridUnit')} * 2);
-  height: calc(${th('gridUnit')} * 2);
-  vertical-align: center;
-  margin-left: ${th('gridUnit')};
-  margin-right: ${th('gridUnit')};
-
-  /* This is not a real border (box-shadow provides that), so not themed as such */
-  border: calc(${th('gridUnit')} / 4) solid white;
-  border-radius: 50%;
-
-  transition: border ${th('transitionDuration')}
-    ${th('transitionTimingFunction')};
-
-  color: ${props => props.color};
-`
-
 const Label = styled.span`
-  display: inline-block;
-  font-family: inherit;
-  font-size: ${th('fontSizeBase')};
-  font-style: italic;
+  ${override('ui.Radio.Label')};
 `
-
-const checking = keyframes`
-  0% {
-    transform: scale(0.8);
-  }
-  20% {
-    transform: scale(1.2);
-  }
-  80% {
-    transform: scale(1);
-  }
-  100% {
-    transform: scale(1);
-  }
+const Input = styled.input`
+  margin-right: ${th('subGridUnit')};
+  ${override('ui.Radio.Input')};
 `
-
 const Root = styled.label`
-  align-items: center;
   cursor: pointer;
+  align-items: center;
   display: ${props => (props.inline ? 'inline-flex' : 'flex')};
-  transition: all ${th('transitionDuration')};
   min-height: calc(${th('gridUnit')} * 3);
-
-  &:not(:last-child) {
-    margin-right: ${props =>
-      props.inline ? `calc(${props.theme.gridUnit} * 3)` : '0'};
-    margin-bottom: 0;
-  }
-
-  ${PseudoInput} {
-    background: ${props => (props.checked ? 'currentcolor' : 'transparent')};
-    box-shadow: 0 0 0 ${th('borderWidth')} currentcolor;
-  }
-
-  &:hover {
-    ${Label} {
-      color: ${props => (props.checked ? 'inherit' : props.theme.colorPrimary)};
-    }
-
-    ${PseudoInput} {
-      animation-name: ${props => (props.checked ? 'none' : checking)};
-      animation-duration: ${th('transitionDuration')};
-      box-shadow: 0 0 0 ${th('borderWidth')}
-        ${props => (props.checked ? 'currentcolor' : props.theme.colorPrimary)};
-    }
-  }
-
-  ${Input}:focus + ${PseudoInput} {
-    box-shadow: 0 0 ${th('borderWidth')} calc(${th('borderWidth')} * 2)
-      ${th('colorPrimary')};
-  }
-
-  color: ${props => props.color};
+  color: ${props => (props.color ? props.color : props.theme.colorText)};
 
   ${override('ui.Radio')};
 `
-
-/* Not used for now
-.root.author {
-  font-family: ${th("fontAuthor")};
-}
-
-.root.author span {
-  font-size: 1.3em;
-  text-transform: lowercase;
-}
-*/
-
 const Radio = ({
   className,
-  color = 'black',
+  color,
   inline,
   name,
   value,
@@ -122,8 +39,10 @@ const Radio = ({
       required={required}
       type="radio"
       value={value}
-    />{' '}
-    <PseudoInput color={color} /> <Label> {label} </Label>{' '}
+    />
+    <Label checked={checked} color={color}>
+      {label}
+    </Label>
   </Root>
 )
 
