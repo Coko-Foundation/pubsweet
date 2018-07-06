615d77fa884855e705240d623907514be68e4d6d
chris <kokosias@yahoo.gr>
2018-07-06 10:35:42 +0300

    fix: have wax back till all feautures are done

--- packages/components/xpub-manuscript/src/components/Manuscript.js
+++ packages/components/xpub-manuscript/src/components/Manuscript.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { withRouter } from 'react-router-dom'
-import { MainEditor } from 'xpub-edit'
+import SimpleEditor from 'wax-editor-react'
+
+// Change to main Editor when everything is done
+// import { MainEditor } from 'xpub-edit'
 
 const Manuscript = ({
   content,
@@ -9,6 +12,19 @@ const Manuscript = ({
   history,
   updateManuscript,
   version,
-}) => <MainEditor onChange={values => true} value={content} />
+}) => (
+  <SimpleEditor
+    content={content}
+    fileUpload={fileUpload}
+    history={history}
+    onSave={source => updateManuscript({ source })}
+    readOnly={version.submitted}
+    trackChanges={false}
+    update={data => updateManuscript(data)}
+    user={currentUser}
+  />
+)
+
+// <MainEditor onChange={values => true} value={content} />
 
 export default withRouter(Manuscript)
