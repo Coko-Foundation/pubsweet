90b223c4c2e4635d4881a22e8b91d640ad79da44
Alexandru Munteanu <alexandru.munt@gmail.com>
2018-08-01 10:11:35 +0300

    test(list-component): add tests

---
+++ packages/ui/test/List.test.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+
+import { List } from '../src/molecules'
+
+describe('<List>', () => {
+  it('renders', () => {
+    const wrapper = shallow(<List />)
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('displays all the items', () => {
+    const items = [
+      { id: 1, name: 'First name' },
+      { id: 2, name: 'Second name' },
+    ]
+    const wrapper = shallow(<List items={items} />)
+    expect(wrapper.children()).toHaveLength(2)
+  })
+})
