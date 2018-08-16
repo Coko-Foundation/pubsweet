d016272ad7d87c0e50ed4af1c25622504794dbed
Mihail Dunaev <mihail.dunaev91@gmail.com>
2018-08-16 17:50:47 +0100

    fix(graphql): return one pubsub only in getPubsub

--- packages/server/src/graphql/pubsub.js
+++ packages/server/src/graphql/pubsub.js
@@ -5,14 +5,18 @@ const { PostgresPubSub } = require('graphql-postgres-subscriptions')
 
 const db = require('../db')
 
+let pubsub
+
 module.exports = {
   /**
    * Pubsub object used in graphql subscriptions
    * to push messages back to the client.
    */
   getPubsub: async () => {
+    if (pubsub) return pubsub
     const client = await db.connect()
-    return new PostgresPubSub({ client })
+    pubsub = new PostgresPubSub({ client })
+    return pubsub
   },
   /**
    * Iterators to listen to
