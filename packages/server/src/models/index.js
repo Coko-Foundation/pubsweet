b2ae124d02a1e7bf8cf8abb258c5868c36372443
Jure Triglav <juretriglav@gmail.com>
2018-09-27 13:51:41 +0200

    fix(server): require components relatively

--- packages/server/src/models/index.js
+++ packages/server/src/models/index.js
@@ -5,17 +5,6 @@ const models = {
   Team: './Team',
 }
 
-const config = require('config')
-
-if (config.has('pubsweet.components')) {
-  config.get('pubsweet.components').forEach(name => {
-    const component = require(name)
-    if (component.model) {
-      module.exports[component.modelName] = component.model
-    }
-  })
-}
-
 Object.keys(models).forEach((key, _) => {
   module.exports[key] = require(models[key])
 })
