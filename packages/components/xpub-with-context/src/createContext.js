5b334ab5e50991ce0a3c2bec7c901c9b124c444a
Giannis Kopanas <jkopanas@gmail.com>
2018-10-05 12:45:20 +0300

    fix(eslint): cleanup dashboard

--- packages/components/xpub-with-context/src/createContext.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const AppContext = React.createContext(null)
-
-export const createContext = data => WrappedComponent => (
-  <AppContext.provider value={data}>
-    <WrappedComponent />
-  </AppContext.provider>
-)
