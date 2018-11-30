9be97dc06cf6ca91d85313055db42b308969fd92
Jure Triglav <juretriglav@gmail.com>
2018-11-30 12:23:28 +0100

    chore: remove validations from config

--- packages/styleguide/config/default.js
+++ packages/styleguide/config/default.js
@@ -1,10 +1,7 @@
-const path = require('path')
-
 module.exports = {
   authsome: {
     mode: 'pubsweet-server/test/helpers/simple_authsome_mode',
   },
-  validations: path.join(__dirname, 'validations'),
   'pubsweet-server': {
     baseUrl: 'http://example.com',
   },
