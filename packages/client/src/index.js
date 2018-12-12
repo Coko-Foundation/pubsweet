da74777bab95e876ef0497e37a59c9ca564098a5
Jure Triglav <juretriglav@gmail.com>
2018-12-12 18:41:50 +0100

    refactor: remove validations from client
    BREAKING CHANGE: This removes the concept of validations from the client (shared with server).
    Validations in general were removed with the switch to BaseModel, which uses JSON Schema and not Joi
    for data validation. While the value of exposing server side validations on the client was never
    realized, we might add something similar back in the future for exposing JSON Schema on the
    client-side, if a clear use-case is presented.

--- packages/client/src/index.js
+++ packages/client/src/index.js
@@ -5,7 +5,6 @@ export {
 } from './components/AuthenticatedComponent'
 export { default as actions } from './actions'
 export { default as reducers } from './reducers'
-export { default as validations } from './validations'
 export { default as withLoader } from './helpers/withLoader'
 export { default as withAuthsome } from './helpers/withAuthsome'
 export { default as withMutationState } from './helpers/withMutationState'
