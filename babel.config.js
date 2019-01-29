08cd269c5e4498d78df0678101d385f5f81bff42
Jure Triglav <juretriglav@gmail.com>
2019-01-29 18:16:14 +1300

    chore: update jest to 24 and babel to 7

---
+++ babel.config.js
@@ -0,0 +1,38 @@
+module.exports = api => {
+  api.cache(true)
+  const presets = [['@babel/preset-env', { targets: { node: true } }]]
+  // const plugins = [ ... ];
+
+  const overrides = [
+    {
+      test: ['./packages/ui', './packages/client'],
+      presets: ['@babel/preset-env', '@babel/preset-react'],
+      plugins: [
+        '@babel/plugin-proposal-class-properties',
+        '@babel/plugin-transform-runtime',
+      ],
+    },
+    {
+      test: ['./packages/ui-toolkit'],
+      presets: ['@babel/preset-env'],
+    },
+    {
+      test: [/\/packages\/components\/(?!model-)/],
+      presets: ['@babel/preset-env', '@babel/preset-react'],
+      plugins: [
+        '@babel/plugin-proposal-class-properties',
+        '@babel/plugin-transform-runtime',
+      ],
+    },
+    {
+      test: ['./packages/components/model-*'],
+      presets: [['@babel/preset-env', { targets: { node: true } }]],
+    },
+  ]
+
+  return {
+    presets,
+    overrides,
+    // plugins
+  }
+}
--- packages/components/model-user/test/jest-setup.js
+++ packages/components/model-user/test/jest-setup.js
@@ -1,7 +1,3 @@
-import Enzyme from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
-import path from 'path'
-
-Enzyme.configure({ adapter: new Adapter() })
+const path = require('path')
 
 process.env.NODE_CONFIG_DIR = path.resolve(__dirname, '..', 'config')
