cd63d9df4e2c3a32caa673963b2dedce76113b38
Giannis Kopanas <jkopanas@gmail.com>
2019-01-30 14:15:02 +0200

    feat(signup): add logo to signup form

---
+++ packages/components/Signup/Signup.jsx
@@ -0,0 +1,68 @@
+import React from 'react'
+import { Field } from 'formik'
+import { override } from '@pubsweet/ui-toolkit'
+import styled from 'styled-components'
+
+import {
+  CenteredColumn,
+  Link,
+  H1,
+  ErrorText,
+  Button,
+  TextField,
+} from '@pubsweet/ui'
+
+const FormContainer = styled.div`
+  ${override('Login.FormContainer')};
+`
+
+const Logo = styled.div`
+  ${override('Login.Logo')};
+`
+
+const UsernameInput = props => (
+  <TextField label="Username" {...props.input} placeholder="Username" />
+)
+
+const EmailInput = props => (
+  <TextField label="Email" {...props.input} placeholder="Email" type="email" />
+)
+const PasswordInput = props => (
+  <TextField
+    label="Password"
+    {...props.input}
+    placeholder="Password"
+    type="password"
+  />
+)
+
+const Signup = ({ error, handleSubmit, logo = null }) => (
+  <CenteredColumn small>
+    {logo && (
+      <Logo>
+        <img alt="pubsweet-logo" src={`${logo}`} />
+      </Logo>
+    )}
+    <FormContainer>
+      <H1>Sign up</H1>
+
+      {error && <ErrorText>{error}</ErrorText>}
+
+      <form onSubmit={handleSubmit}>
+        <Field component={UsernameInput} name="username" />
+        <Field component={EmailInput} name="email" />
+        <Field component={PasswordInput} name="password" />
+        <Button primary type="submit">
+          Sign up
+        </Button>
+      </form>
+
+      <div>
+        <span>Already have an account? </span>
+        <Link to="/login">Login</Link>
+      </div>
+    </FormContainer>
+  </CenteredColumn>
+)
+
+export default Signup
