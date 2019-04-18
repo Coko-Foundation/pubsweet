292720dcf1b05acd36758f2e0f755fd5affe54bd
Jure Triglav <juretriglav@gmail.com>
2019-04-18 11:46:14 +0200

    feat(server): upgrade integration with apollo server 2.0

--- packages/server/src/graphql/api.js
+++ packages/server/src/graphql/api.js
@@ -1,32 +1,32 @@
-const express = require('express')
 const passport = require('passport')
-const { graphqlExpress, graphiqlExpress } = require('apollo-server-express')
-const { apolloUploadExpress } = require('apollo-upload-server')
+const { ApolloServer } = require('apollo-server-express')
 const logger = require('@pubsweet/logger')
 const errors = require('@pubsweet/errors')
 
 const config = require('config')
 
-const graphqlSchema = require('./schema')
+const schema = require('./schema')
 const connectors = require('../connectors')
 
 const authBearerAndPublic = passport.authenticate(['bearer', 'anonymous'], {
   session: false,
 })
-const router = new express.Router()
 
 const helpers = require('../helpers/authorization')
 
 const hostname = config.has('pubsweet-server.hostname')
   ? config.get('pubsweet-server.hostname')
   : 'localhost'
-router.use(
-  '/graphql',
-  authBearerAndPublic,
-  apolloUploadExpress(),
-  graphqlExpress(req => ({
-    schema: graphqlSchema,
-    context: { user: req.user, connectors, helpers },
+
+const api = app => {
+  app.use('/graphql', authBearerAndPublic)
+  const server = new ApolloServer({
+    schema,
+    context: ({ req, res }) => ({
+      user: req.user,
+      connectors,
+      helpers,
+    }),
     formatError: err => {
       const error = err.originalError || err
 
@@ -35,33 +35,29 @@ router.use(
       const isPubsweetDefinedError = Object.values(errors).some(
         pubsweetError => error instanceof pubsweetError,
       )
-
       // err is always a GraphQLError which should be passed to the client
-      if (err.originalError && !isPubsweetDefinedError)
+      if (
+        Object.keys(err.originalError).length !== 0 &&
+        !isPubsweetDefinedError
+      )
         return {
           name: 'Server Error',
           message: 'Something went wrong! Please contact your administrator',
         }
 
       return {
-        name: error.name,
+        name: error.name || 'GraphQLError',
         message: error.message,
+        extensions: {
+          code: err.extensions.code,
+        },
       }
     },
-  })),
-)
-if (
-  config.has('pubsweet-server.graphiql') &&
-  config.get('pubsweet-server.graphiql')
-) {
-  router.get(
-    '/graphiql',
-    authBearerAndPublic,
-    graphiqlExpress({
-      endpointURL: '/graphql',
-      subscriptionsEndpoint: `ws://${hostname}:3000/subscriptions`,
-    }),
-  )
+    playground: {
+      subscriptionEndpoint: `ws://${hostname}:3000/subscriptions`,
+    },
+  })
+  server.applyMiddleware({ app })
 }
 
-module.exports = router
+module.exports = api
