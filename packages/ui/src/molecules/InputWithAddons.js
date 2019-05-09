b60a728bfbcafef418b292c8404467703abcb148
Erez Odier <erez.odier@gmail.com>
2019-05-09 14:47:55 +0100

    fix(peoplepicker): fixed styling of InputWithAddons

--- packages/ui/src/molecules/InputWithAddons.js
+++ packages/ui/src/molecules/InputWithAddons.js
@@ -1,20 +1,57 @@
 import React from 'react'
-
+import styled from 'styled-components'
+import { th } from '@pubsweet/ui-toolkit'
 import { Button, TextField } from '../atoms'
 
+const InputContainer = styled.div`
+  display: flex;
+
+  & > div {
+    flex: 1;
+  }
+`
+
+const StyledTextField = styled(TextField)`
+  margin-bottom: 0;
+
+  & input {
+    border-top-right-radius: 0;
+    border-bottom-right-radius: 0;
+  }
+`
+
+const AddOnButton = styled(Button)`
+  border-radius: 0px;
+  border: ${th('borderWidth')} ${th('borderStyle')} ${th('colorBorder')};
+  border-right-width: 0;
+  fill: ${th('colorTextReverse')};
+  line-height: 0;
+  min-width: 0;
+  padding: ${th('space.1')};
+  margin: 0;
+  width: ${th('space.5')};
+
+  &:last-child {
+    border-right-width: ${th('borderWidth')}
+    border-radius: 0 ${th('borderRadius')} ${th('borderRadius')} 0;
+  }
+`
+
 const InputWithAddons = ({ addons, inputProps, ...props }) => (
-  <div {...props}>
-    <TextField {...inputProps} />
+  <InputContainer {...props}>
+    <div>
+      <StyledTextField {...inputProps} />
+    </div>
     {addons.map((itemConfig, index) => (
-      <Button
+      <AddOnButton
         key={itemConfig.buttonProps.key || index}
         {...itemConfig.buttonProps}
       >
         {' '}
         {itemConfig.icon}{' '}
-      </Button>
+      </AddOnButton>
     ))}
-  </div>
+  </InputContainer>
 )
 
 export default InputWithAddons
