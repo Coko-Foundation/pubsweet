8f81e341ae3e291909b7ebfa16681c6f343a1fa8
Erez Odier <erez.odier@gmail.com>
2019-05-10 16:54:29 +0100

    fix: put back the props spread of RemoveButton in SelectedItem

--- components/client/component-people-picker/src/SelectedItem.js
+++ components/client/component-people-picker/src/SelectedItem.js
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 import { th } from '@pubsweet/ui-toolkit'
 import { Icon } from '@pubsweet/ui'
 
-const RemoveButton = ({ onClick }) => <Icon onClick={onClick}>X</Icon>
+const RemoveButton = props => <Icon {...props}>X</Icon>
 
 const StyledRemoveButton = styled(RemoveButton)`
   margin-left: ${th('space.1')};
