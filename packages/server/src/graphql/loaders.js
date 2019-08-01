3be6bc281e3a5b77e365a0de271086af398186c9
Jure Triglav <juretriglav@gmail.com>
2019-08-01 19:51:52 +0200

    feat: use modelLoaders for model-specific loaders

--- packages/server/src/graphql/loaders.js
+++ packages/server/src/graphql/loaders.js
@@ -22,17 +22,22 @@ module.exports = () => {
   const loaders = {}
 
   components.forEach(component => {
-    if (component.model && component.modelName) {
+    if (component.model && component.modelName && component.modelLoaders) {
       // Sets up the default loader, that gets model instances by id
       // You can use it with e.g. context.loaders.User.load(id)
       loaders[component.modelName] = defaultLoader(component.model)
+      Object.keys(component.modelLoaders).forEach(loaderName => {
+        loaders[component.modelName][loaderName] = new DataLoader(
+          component.modelLoaders[loaderName],
+        )
+      })
     } else if (component.models) {
       // If there are multiple models specified in a single component
       // each can specify its own loaders
       component.models.forEach(model => {
         loaders[model.modelName] = defaultLoader(model.model)
-        if (model.loaders) {
-          Object.keys(model.loaders).forEach(loaderName => {
+        if (model.modelLoaders) {
+          Object.keys(model.modelLoaders).forEach(loaderName => {
             loaders[model.modelName][loaderName] = new DataLoader(
               model.loaders[loaderName],
             )
