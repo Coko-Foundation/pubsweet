29d7d44f19a92dfb8db372da2eaa0169ec3568c1
Jure Triglav <juretriglav@gmail.com>
2019-08-21 00:38:50 +0200

    feat(cli): introduce start:client and start:server

--- packages/cli/cli/start-server.js
+++ packages/cli/cli/start-server.js
@@ -1,11 +1,20 @@
+const config = require('config')
 const logger = require('@pubsweet/logger')
+const { execSync } = require('child_process')
 
 const requireRelative = m =>
   require(require.resolve(m, { paths: [process.cwd()] }))
 
 module.exports = async argsOverride => {
-  logger.info("Starting PubSweet app's server")
-  const { startServer } = requireRelative('pubsweet-server')
+  const env = config.util.getEnv('NODE_ENV') // process.env.NODE_ENV
+  logger.info(`Starting PubSweet app's server (${env})`)
 
-  startServer()
+  if (env === 'development') {
+    const serverPath = require.resolve('pubsweet-server/src/start')
+    execSync(`yarn node-dev ${serverPath}`, { stdio: 'inherit' })
+  } else {
+    const { startServer } = requireRelative('pubsweet-server')
+
+    startServer()
+  }
 }
