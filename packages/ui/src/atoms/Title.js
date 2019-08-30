0f0353f0a959f570c512f1ed79c7b8823d5998f1
Audrey Hamelers <hamelers@ebi.ac.uk>
2019-08-30 15:13:12 +0100

    refactor(ui): deprecate Title
    Title should be abandoned in favor of Heading
    
    BREAKING CHANGE: Anyone applying styles using a div css selector will have to change to using an
    h-level css selecto

--- packages/ui/src/atoms/Title.js
+++ packages/ui/src/atoms/Title.js
@@ -1,38 +1,6 @@
-import styled from 'styled-components'
-import { th, override } from '@pubsweet/ui-toolkit'
+import React from 'react'
+import { Heading } from './Heading'
 
-// Deprecated! Please use Heading instead
+const Title = ({ level }) => <Heading level={level} />
 
-const fontSize = ({ theme, level = 1 }) =>
-  ({
-    1: theme.fontSizeHeading1,
-    2: theme.fontSizeHeading2,
-    3: theme.fontSizeHeading3,
-    4: theme.fontSizeHeading4,
-    5: theme.fontSizeHeading5,
-    6: theme.fontSizeHeading6,
-  }[level])
-
-const lineHeight = ({ theme, level = 1 }) =>
-  ({
-    1: theme.lineHeightHeading1,
-    2: theme.lineHeightHeading2,
-    3: theme.lineHeightHeading3,
-    4: theme.lineHeightHeading4,
-    5: theme.lineHeightHeading5,
-    6: theme.lineHeightHeading6,
-  }[level])
-
-const Title = styled.div`
-  font-size: ${fontSize};
-  font-family: ${th('fontHeading')};
-  line-height: ${lineHeight};
-  margin-bottom: calc(${th('gridUnit')} * 3);
-
-  ${override('ui.Title')};
-`
-
-/**
- * @component
- */
 export default Title
