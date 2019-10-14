b2189239014a213cd2593da837bcd777a535c73e
Giannis Kopanas <jkopanas@gmail.com>
2019-10-14 19:31:06 +0300

    fix(xsweet): add wait for it as linux package

--- components/server/job-xsweet/src/service_run.sh
+++ components/server/job-xsweet/src/service_run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -x
-exec ./src/wait-for-it.sh $WAIT_SERVICE_PORT --strict --timeout=$WAIT_TIMEOUT -- node ./src/xsweet.js
+wait-for-it $WAIT_SERVICE_PORT --strict --timeout=$WAIT_TIMEOUT -- node ./src/xsweet.js
