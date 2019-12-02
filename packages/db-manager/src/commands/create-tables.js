b4b9c951b712e363c3e977a0c8a3243c489c5aa4
Jure Triglav <juretriglav@gmail.com>
2019-12-02 15:00:42 +0100

    fix: database reset should include pgboss features

--- packages/db-manager/src/commands/create-tables.js
+++ packages/db-manager/src/commands/create-tables.js
@@ -4,7 +4,7 @@ const migrate = require('./migrate')
 
 const createTables = async clobber => {
   const { rows } = await db.raw(`
-    SELECT tablename
+    SELECT tablename, schemaname
     FROM pg_tables
     WHERE schemaname = 'public' OR schemaname = 'pgboss'
   `)
@@ -13,9 +13,14 @@ const createTables = async clobber => {
     if (clobber) {
       logger.info('Overwriting existing database due to clobber option')
       // TODO this is dangerous, change it
-      const dropQuery = rows
-        .map(row => `DROP TABLE "${row.tablename}" CASCADE`)
-        .join(';')
+      let dropQuery = rows.map(
+        row => `DROP TABLE ${row.schemaname}.${row.tablename} CASCADE`,
+      )
+
+      // Also delete the pgboss.job_state type
+      dropQuery.push('DROP TYPE IF EXISTS pgboss.job_state')
+      dropQuery = dropQuery.join('; ')
+
       await db.raw(dropQuery)
     } else {
       logger.error(
