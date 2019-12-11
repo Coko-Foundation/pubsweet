b96cdf9663d48480bcb2940938d2b0c661e729cf
Jure Triglav <juretriglav@gmail.com>
2019-12-11 14:14:25 +0100

    chore: remove big xpub components (they now live in simplej)

--- components/client/component-xpub-dashboard/src/components/metadata/MetadataStreamLined.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-
-const MetadataStreamLined = ({ streamlinedReview }) =>
-  streamlinedReview === 'yes' ? <span>Streamlined</span> : null
-
-export default MetadataStreamLined
