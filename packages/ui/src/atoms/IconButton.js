c91b9647015a8bf8c85705fcc6ec809e2cc53afb
Sidorela Uku <uku.sidorela@gmail.com>
2020-02-13 08:37:36 +0100

    fix: added more props to IconButton

--- packages/ui/src/atoms/IconButton.js
+++ packages/ui/src/atoms/IconButton.js
@@ -1,30 +1,36 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { th } from '@pubsweet/ui-toolkit'
-import styled from 'styled-components'
 import { Button, Icon } from '../atoms'
 
-const UIButton = styled(Button)`
-  line-height: calc(${th('gridUnit')} * 2);
-`
-
-const IconButton = ({ icon, ...props }) => (
-  <UIButton {...props}>
+const IconButton = ({ icon, children, iconPosition, ...props }) => (
+  <Button {...props}>
+    {children != null && iconPosition === 'end' && <span>{children}</span>}
     <Icon
       color={props.primary ? th('colorTextReverse') : th('colorPrimary')}
-      data-testid="iconButton"
+      size={2}
       {...props}
     >
       {icon}{' '}
     </Icon>
-  </UIButton>
+    {children != null && iconPosition === 'start' && <span>{children}</span>}
+  </Button>
 )
 
 IconButton.propTypes = {
+  /** The contents of the button (text, icon etc.) */
+  children: PropTypes.node,
   /** Icon name (An icon name, from the Feather icon set.) */
   icon: PropTypes.string,
+  /** Icon Position (Defines the position of the icon (if is is at the start of the button , or at the end)) */
+  iconPosition: PropTypes.oneOf(['start', 'end']),
   /** Makes button a primary button */
   primary: PropTypes.bool,
+}
+
+IconButton.defaultProps = {
+  children: null,
+  iconPosition: 'start',
 }
 
 export default IconButton
--- packages/ui/src/atoms/IconButton.md
+++ packages/ui/src/atoms/IconButton.md
@@ -9,3 +9,17 @@ A non-primary IconButton
 ```js
 <IconButton icon="plus" />
 ```
+
+Button with Text and Icon at the end
+
+```js
+<IconButton icon="plus" iconPosition="end">
+  Add
+</IconButton>
+```
+
+Button with Icon before Text
+
+```js
+<IconButton icon="plus">Add</IconButton>
+```
