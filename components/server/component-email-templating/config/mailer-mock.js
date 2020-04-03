55f0c6b3920c8dbb02c0b6ed2ca5dd39e27ddb9d
Andrei Cioromila <andrei.cioromila@thinslices.com>
2020-04-03 19:29:16 +0300

    test(component-email-template): configure tests

--- components/server/component-email-templating/config/mailer-mock.js
+++ components/server/component-email-templating/config/mailer-mock.js
@@ -1,13 +1,5 @@
 module.exports = {
   transport: {
-    name: 'mailer-mock',
-    version: '0.1.0',
-    send: (mail, callback) => {
-      const input = mail.message.createReadStream()
-      input.pipe(process.stdout)
-      input.on('end', () => {
-        callback(null, true)
-      })
-    },
+    jsonTransport: true,
   },
 }
