fe9abcf7799c0ea39336c9246688b5d82ecb73b8
Yannis Barlas <yannisbarlas@gmail.com>
2020-11-17 21:50:07 +0200

    feat(component-login): upgrade to latest apollo client
    BREAKING CHANGE: Major apollo client version changed

--- components/client/component-login/test/LoginContainer.test.jsx
+++ components/client/component-login/test/LoginContainer.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { mount } from 'enzyme'
-import { MockedProvider } from '@apollo/react-testing'
+import { MockedProvider } from '@apollo/client/testing'
 import { ThemeProvider } from 'styled-components'
 import { MemoryRouter, Route, Switch } from 'react-router-dom'
 
