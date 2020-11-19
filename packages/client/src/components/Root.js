66e590371f05404c2e2839cb1ade949397d9af88
Yannis Barlas <yannisbarlas@gmail.com>
2020-11-19 20:22:55 +0200

    fix(client): allow client env variables to work for graphql and websockets

--- packages/client/src/components/Root.js
+++ packages/client/src/components/Root.js
@@ -19,7 +19,13 @@ import { createUploadLink } from 'apollo-upload-client'
 
 import StyleRoot from '../helpers/StyleRoot'
 
-const serverUrl = process.env.SERVER_URL || window.location.host
+const serverProtocol = process.env.SERVER_PROTOCOL
+const serverHost = process.env.SERVER_HOST
+const serverPort = process.env.SERVER_PORT
+
+let serverUrl = `${serverHost}${serverPort ? `:${serverPort}` : ''}`
+const serverUrlWithProtocol = `${serverProtocol}://${serverUrl}`
+if (!serverUrl) serverUrl = window.location.host
 
 // See https://github.com/apollographql/apollo-feature-requests/issues/6#issuecomment-465305186
 export function stripTypenames(obj) {
@@ -44,7 +50,7 @@ export function stripTypenames(obj) {
 // return the desired config.
 const makeApolloClient = (makeConfig, connectToWebSocket) => {
   const uploadLink = createUploadLink({
-    uri: `${serverUrl}/graphql`,
+    uri: `${serverUrlWithProtocol}/graphql`,
   })
 
   const authLink = setContext((_, { headers }) => {
@@ -67,7 +73,7 @@ const makeApolloClient = (makeConfig, connectToWebSocket) => {
   let link = ApolloLink.from([removeTypename, authLink, uploadLink])
 
   if (connectToWebSocket) {
-    const wsProtocol = window.location.protocol === 'https:' ? 'wss' : 'ws'
+    const wsProtocol = serverProtocol === 'https' ? 'wss' : 'ws'
     const wsLink = new WebSocketLink({
       uri: `${wsProtocol}://${serverUrl}/subscriptions`,
       options: {
